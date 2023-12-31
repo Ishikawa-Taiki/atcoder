9a07d0a0beed5c760fea796e27382497c48527c6
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-01 07:16:12 +0900

    atcoder-cliのテンプレート系ファイルをdotfilesから本リポジトリへ移行 (#3)
    * テンプレート系ファイルをdotfilesから本リポジトリへ移行
    
    * setup.shの使い方を追記(&フォーマット)
    
    * テンプレートの追加方法を記載
    
    * Swiftの.gitignoreを足し忘れていたので追加

---
+++ atcoder-cli/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# ユーザー設定
+CUSTOM_SETTINGS_DIRECTORY=`pwd`
+APP_SETTINGS_DIRECTORY=$HOME/Library/Preferences/atcoder-cli-nodejs
+
+# 共通の設定ファイルをatcoder-cliの設定フォルダ配下へリンクする
+ln -sf "$CUSTOM_SETTINGS_DIRECTORY/config.json"		"$APP_SETTINGS_DIRECTORY/config.json"
+
+# 本ディレクトリ配下の全てのディレクトリをatcoder-cliの設定フォルダ配下へリンクする
+for item in `ls -d */ | cut -d' ' -f 1`; do
+	ln -nsf "${CUSTOM_SETTINGS_DIRECTORY}/${item%/}"		"${APP_SETTINGS_DIRECTORY}/${item%/}"
+done
