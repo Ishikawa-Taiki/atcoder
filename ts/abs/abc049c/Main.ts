8a4e0b2396a9c2480610044a363972f2a14118a1
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-02 21:52:50 +0900

    AC https://atcoder.jp/contests/abs/submissions/48988432

--- ts/abs/abc049c/Main.ts
+++ ts/abs/abc049c/Main.ts
@@ -2,24 +2,42 @@ import * as fs from "fs";
 
 function main() {
   const s = IOUtil.readAllLines()[0];
-  const found = findMatchString(s, "");
-  console.log(found ? "YES" : "NO");
+  // 貪欲法で解くのが良いらしいので書いてみる(反転した文字列で確認していく)
+  let target = StringUtil.reverse(s);
+  const reverseWords = ["dream", "dreamer", "erase", "eraser"].map(
+    StringUtil.reverse
+  );
+  do {
+    const searchResult = reverseWords.find((w) => target.startsWith(w));
+    if (!searchResult) {
+      break;
+    }
+    target = target.substring(searchResult.length);
+  } while (target.length > 0);
+  console.log(target.length === 0 ? "YES" : "NO");
 }
 
-const WORDS = ["dream", "dreamer", "erase", "eraser"];
-// currentから始まるtargetの文字列を探し続ける
-const findMatchString = (target: string, current: string): boolean => {
-  if (target === current) {
-    return true;
-  }
-  if (target.length < current.length) {
-    return false;
-  }
-  return WORDS.reduce(
-    (acc, v) => acc || findMatchString(target, `${current}${v}`),
-    false
-  );
-};
+// TLEが解消できないので書き直し
+// function main() {
+//   const s = IOUtil.readAllLines()[0];
+//   const found = findMatchString(s, "");
+//   console.log(found ? "YES" : "NO");
+// }
+
+// const WORDS = ["dream", "dreamer", "erase", "eraser"];
+// // currentから始まるtargetの文字列を探し続ける
+// const findMatchString = (target: string, current: string): boolean => {
+//   if (target === current) {
+//     return true;
+//   }
+//   if (target.length < current.length) {
+//     return false;
+//   }
+//   return WORDS.reduce(
+//     (acc, v) => acc || findMatchString(target, `${current}${v}`),
+//     false
+//   );
+// };
 
 export module IOUtil {
   /**
@@ -28,6 +46,12 @@ export module IOUtil {
    */
   export function readAllLines() {
     return fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+  }
+}
+
+export module StringUtil {
+  export function reverse(s: string): string {
+    return s.split("").reverse().join("");
   }
 }
 
