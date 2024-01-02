c05bb45c3efd060c35efbaa6471c119ddf28872c
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-02 13:26:38 +0900

    提出 https://atcoder.jp/contests/abs/submissions/48980442

---
+++ ts/abs/abc085b/Main.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+function main() {
+  // template
+  const lines = IOUtil.readAllLines().map(Number);
+  const itemCount = lines[0] + 1;
+  const sortedDiameters = lines.slice(1, itemCount);
+  // Set利用で要素の重複を省く
+  const result = new Set(sortedDiameters).size;
+  console.log(result);
+}
+
+export module IOUtil {
+  /**
+   * 全ての標準入力を読み取って返却する
+   * @returns 行毎の配列
+   */
+  export function readAllLines() {
+    return fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+  }
+}
+
+main();
