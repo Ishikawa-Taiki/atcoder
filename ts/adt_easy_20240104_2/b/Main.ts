23becd78d4e4f25794890f51df3c751af5958f25
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-06 12:32:13 +0900

    WA https://atcoder.jp/contests/adt_easy_20240104_2/submissions/49047694

---
+++ ts/adt_easy_20240104_2/b/Main.ts
@@ -0,0 +1,30 @@
+// © 2024 Ishikawa-Taiki
+import * as fs from "fs";
+
+function main() {
+  const [a, b] = IOUtil.readAllLines()[0].split(/\s/).map(Number).sort();
+  const result = b == a * 2 || b == a * 2 + 1 ? "Yes" : "No";
+  console.log(result);
+}
+
+export module IOUtil {
+  /**
+   * 全ての標準入力を読み取って返却する
+   * @returns 行毎の配列
+   */
+  export function readAllLines() {
+    return fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+  }
+}
+
+export module StringUtil {
+  /**
+   * 文字列を反転する
+   * @returns 反転した文字列
+   */
+  export function reverse(s: string): string {
+    return s.split("").reverse().join("");
+  }
+}
+
+main();
