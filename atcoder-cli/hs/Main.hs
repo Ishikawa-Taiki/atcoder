700253f3d8e6275f45c8071b0bf999f7be852ff9
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-09 01:01:22 +0900

    作成途中のhsライブラリを仮登録

--- atcoder-cli/hs/Main.hs
+++ atcoder-cli/hs/Main.hs
@@ -2,6 +2,11 @@
 -- © 2024 Ishikawa-Taiki
 
 module Main (main) where
+import Data.Bool (bool)
+import qualified Data.List as L
+import qualified Data.ByteString.Char8 as BS
+import Data.Char (isSpace)
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = do
@@ -10,3 +15,22 @@ main = do
 
 solve :: [Int] -> Int
 solve xs = undefined
+
+{- Library -}
+-- IO 出力系
+printYesNo :: Bool -> IO ()
+printYesNo = putStrLn . bool "No" "Yes"
+
+printListWithSpace :: (Show a) => [a] -> IO ()
+printListWithSpace = putStrLn . unwords . map show
+
+printListWithLn :: (Show a) => [a] -> IO ()
+printListWithLn = putStr . unlines . map show
+
+-- IO 入力系
+getInt :: IO Int
+getInt = fst . fromJust . BS.readInt <$> BS.getLine
+getIntList :: IO [Int]
+getIntList = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+-- getIntMatrix :: IO [[Int]]
+-- getIntMatrix = map (fst . fromJust . BS.readInt . BS.words) $ BS.lines <$> BS.getContents
--- hs/NOTE.md
+++ hs/NOTE.md
@@ -4,3 +4,12 @@
 
 runghc で直接実行可能。
 厳密にやるには別の方法の方が良い様子。
+
+hackage
+https://hackage.haskell.org/package/base-4.17.1.0/docs/doc-index.html
+
+AtCoder ガチ言語 Haskell 🔥
+https://zenn.dev/toyboot4e/books/seriously-haskell
+
+Haskell で AtCoder の問題を解く（入力の高速化編）
+https://qiita.com/hnw/items/3f7d27b742c5a1a99a9a
