1528f9706b24c2b7a3d103dbef2cc2946c4ae3c7
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-10 00:51:48 +0900

    hs入出力ライブラリを仮実装

--- atcoder-cli/hs/Main.hs
+++ atcoder-cli/hs/Main.hs
@@ -1,36 +1,85 @@
 #!/usr/bin/env runghc
+
 -- © 2024 Ishikawa-Taiki
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Main (main) where
+
 import Data.Bool (bool)
-import qualified Data.List as L
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import Data.Char (isSpace)
+import qualified Data.List as L
 import Data.Maybe (fromJust)
 
 main :: IO ()
 main = do
-  xs <- map read . words <$> getLine :: IO [Int]
-  print $ solve xs
+  xs <- getLineToIntArray
+  printArrayWithLn xs
 
 solve :: [Int] -> Int
 solve xs = undefined
 
 {- Library -}
+-- データ変換共通
+boolToYesNo :: Bool -> String
+boolToYesNo = bool "No" "Yes"
+
+arrayToTuple2 :: [a] -> (a, a)
+arrayToTuple2 (a : b : _) = (a, b)
+
+arrayToTuple3 :: [a] -> (a, a, a)
+arrayToTuple3 (a : b : c : _) = (a, b, c)
+
+bsToInt :: ByteString -> Int
+bsToInt = fst . fromJust . BS.readInt
+
+bsToIntList :: ByteString -> [Int]
+bsToIntList = fmap bsToInt . BS.words
+
+bsToIntTuple2 :: ByteString -> (Int, Int)
+bsToIntTuple2 = arrayToTuple2 . bsToIntList
+
+bsToIntTuple3 :: ByteString -> (Int, Int, Int)
+bsToIntTuple3 = arrayToTuple3 . bsToIntList
+
+bsToIntMatrix :: ByteString -> [[Int]]
+bsToIntMatrix = fmap bsToIntList . BS.lines
+
+bsToIntTuples2 :: ByteString -> [(Int, Int)]
+bsToIntTuples2 = fmap (arrayToTuple2 . bsToIntList) . BS.lines
+
+bsToIntTuples3 :: ByteString -> [(Int, Int, Int)]
+bsToIntTuples3 = fmap (arrayToTuple3 . bsToIntList) . BS.lines
+
 -- IO 出力系
 printYesNo :: Bool -> IO ()
-printYesNo = putStrLn . bool "No" "Yes"
+printYesNo = putStrLn . boolToYesNo
 
-printListWithSpace :: (Show a) => [a] -> IO ()
-printListWithSpace = putStrLn . unwords . map show
+printArrayWithSpace :: (Show a) => [a] -> IO ()
+printArrayWithSpace = putStrLn . unwords . fmap show
 
-printListWithLn :: (Show a) => [a] -> IO ()
-printListWithLn = putStr . unlines . map show
+printArrayWithLn :: (Show a) => [a] -> IO ()
+printArrayWithLn = putStr . unlines . fmap show
 
 -- IO 入力系
-getInt :: IO Int
-getInt = fst . fromJust . BS.readInt <$> BS.getLine
-getIntList :: IO [Int]
-getIntList = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
--- getIntMatrix :: IO [[Int]]
--- getIntMatrix = map (fst . fromJust . BS.readInt . BS.words) $ BS.lines <$> BS.getContents
+getLineToInt :: IO Int
+getLineToInt = bsToInt <$> BS.getLine
+
+getLineToIntArray :: IO [Int]
+getLineToIntArray = bsToIntList <$> BS.getLine
+
+getLineToIntTuple2 :: IO (Int, Int)
+getLineToIntTuple2 = bsToIntTuple2 <$> BS.getLine
+
+getLineToIntTuple3 :: IO (Int, Int, Int)
+getLineToIntTuple3 = bsToIntTuple3 <$> BS.getLine
+
+getContentsToIntMatrix :: IO [[Int]]
+getContentsToIntMatrix = bsToIntMatrix <$> BS.getContents
+
+getContentsToIntTuples2 :: IO [(Int, Int)]
+getContentsToIntTuples2 = bsToIntTuples2 <$> BS.getContents
+
+getContentsToIntTuples3 :: IO [(Int, Int, Int)]
+getContentsToIntTuples3 = bsToIntTuples3 <$> BS.getContents
