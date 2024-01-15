e294a8e3c566b4d73f13872c836ae1b32d5ffa8f
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-15 23:33:30 +0900

    2引数関数と1引数関数の合成

--- hs/_trial/Point-Free-Style.hs
+++ hs/_trial/Point-Free-Style.hs
@@ -1,12 +1,31 @@
 -- © 2024 Ishikawa-Taiki
+{-# LANGUAGE BangPatterns #-}
+
 module Main (main) where
+
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  print $ arg1Test1 . arg1Test1 $ 5
+  print "---  ---"
+  print $ arg1Add1 $ arg1Add1 0 -- 2:1引数関数の順次呼び出し
+  print $ arg1Add1 . arg1Add1 $ 0 -- 2:1引数関数を合成して呼び出し
+  print $ arg1Add1 $ arg2Mul 2 3 -- 7:2引数関数呼び出し後の1引数関数の呼び出し
+  print $ ((arg1Add1 .) . arg2Mul) 2 3 -- 7:2引数関数呼び出し後の1引数関数の呼び出し
 
-arg1Test1 :: Int -> Int
-arg1Test1 x = x + 1
+-- 1引数関数
+arg1Add1 :: Int -> Int
+arg1Add1 = (1 +)
 
-arg1Test2 :: Int -> Int
-arg1Test2 x = x + 2
+-- 2引数関数
+arg2Mul :: Int -> Int -> Int
+arg2Mul = (*)
+
+-- 動作確認用
+debugProxy :: (Show a) => a -> a
+debugProxy value =
+  let !_ = debug "[DebugProxy]" value
+   in value
+
+debug :: (Show a) => String -> a -> ()
+debug key value = trace (key ++ " : " ++ show value) ()
