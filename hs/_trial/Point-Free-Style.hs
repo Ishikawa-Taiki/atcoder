d06bc27587f88a4914d7c84122e4a796bc07a95e
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-16 13:50:07 +0900

    関数合成の学習

--- hs/_trial/Point-Free-Style.hs
+++ hs/_trial/Point-Free-Style.hs
@@ -1,5 +1,8 @@
 -- © 2024 Ishikawa-Taiki
 {-# LANGUAGE BangPatterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
 
 module Main (main) where
 
@@ -7,11 +10,18 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  print "---  ---"
+  putStrLn "--- 1arg 1arg ---"
   print $ arg1Add1 $ arg1Add1 0 -- 2:1引数関数の順次呼び出し
   print $ arg1Add1 . arg1Add1 $ 0 -- 2:1引数関数を合成して呼び出し
+  putStrLn "--- 2arg 1arg ---"
   print $ arg1Add1 $ arg2Mul 2 3 -- 7:2引数関数呼び出し後の1引数関数の呼び出し
   print $ ((arg1Add1 .) . arg2Mul) 2 3 -- 7:2引数関数と1引数関数を合成して呼び出し
+  putStrLn "--- Functor ---"
+  print $ fmap arg1Add1 $ arg1Just 1 -- Just 2:Functorに包む関数と1引数関数の順次呼び出し
+  print $ arg1Add1 <$> arg1Just 1 -- Just 2:Functorに包む関数と1引数関数の順次呼び出し
+  print $ arg1Add1 <$> arg1Add1 $ 0 -- 2:1引数関数を合成して呼び出し(関数もFunctor)
+  putStrLn "--- Applicative ---"
+  print $ (+) <$> arg1Just 1 <*> arg1Just 2 -- Just 3:複数の引数を取る関数に複数の値を適用する
 
 -- 1引数関数
 arg1Add1 :: Int -> Int
@@ -20,6 +30,10 @@ arg1Add1 = (1 +)
 -- 2引数関数
 arg2Mul :: Int -> Int -> Int
 arg2Mul = (*)
+
+-- 1引数関数(Functor)
+arg1Just :: Int -> Maybe Int
+arg1Just = Just
 
 -- 動作確認用
 debugProxy :: (Show a) => a -> a
