3963fbce3e34e4d6aa325c8a610d6c7ed25e1fd4
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-01-27 22:40:55 +0900

    WA https://atcoder.jp/contests/abc338/submissions/49740035

---
+++ hs/abc338/c/Main.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE CPP #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas -Wno-incomplete-patterns -Wno-unused-imports -Wno-unused-top-binds -Wno-name-shadowing -Wno-unused-matches #-}
+
+-- © 2024 Ishikawa-Taiki
+module Main (main) where
+
+import Data.Bool (bool)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Char (isSpace)
+import Data.List (sort)
+import qualified Data.List as L
+import Data.Maybe (fromJust)
+import Data.Set (fromList, toList)
+import Debug.Trace (trace)
+import GHC.Float (int2Float)
+
+main :: IO ()
+main = do
+  n <- getLineToInt
+  q <- getLineToIntArray
+  a <- getLineToIntArray
+  b <- getLineToIntArray
+  print $ solve q a b
+
+solve :: [Int] -> [Int] -> [Int] -> Int
+solve q a b =
+  let aMax = debugProxy $ makeMax q a
+      bMax = debugProxy $ makeMax q b
+      candidate = debugProxy $ [(aNum, bNum) | aNum <- [aMax, aMax -1 .. 0], bNum <- [bMax, bMax -1 .. 0], canMake q ((* aNum) <$> a) ((* bNum) <$> b)]
+   in if null candidate then 0 else maximum $ uncurry (+) <$> candidate
+
+-- 食材リストと作成コストを渡し、料理を作れる数を返却する
+-- makeMax [800,300] [100,100] -> 3
+-- makeMax [800,300] [200,10] -> 4
+--  0割を生んでしまったので一旦１にマッピング
+makeMax :: [Int] -> [Int] -> Int
+makeMax q cost = minimum $ uncurry div <$> zip q ((\x -> if x == 0 then 1 else x) <$> cost)
+
+-- 食材リストとA,Bそれぞれのコストを渡し、料理を作れるかどうかを返却する
+canMake :: [Int] -> [Int] -> [Int] -> Bool
+canMake q aCost bCost =
+  let cost = (+) <$> aCost <*> bCost
+   in all (>= 0) $ (-) <$> q <*> cost
+
+{- Library -}
+-- データ変換共通
+boolToYesNo :: Bool -> String
+boolToYesNo = bool "No" "Yes"
+
+fst3 :: (a, b, c) -> a
+fst3 (a, _, _) = a
+
+snd3 :: (a, b, c) -> b
+snd3 (_, b, _) = b
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, c) = c
+
+arrayToTuple2 :: [a] -> (a, a)
+arrayToTuple2 (a : b : _) = (a, b)
+
+arrayToTuple3 :: [a] -> (a, a, a)
+arrayToTuple3 (a : b : c : _) = (a, b, c)
+
+bsToInt :: ByteString -> Int
+bsToInt = fst . fromJust . BS.readInt
+
+bsToIntList :: ByteString -> [Int]
+bsToIntList = fmap bsToInt . BS.words
+
+bsToIntTuple2 :: ByteString -> (Int, Int)
+bsToIntTuple2 = arrayToTuple2 . bsToIntList
+
+bsToIntTuple3 :: ByteString -> (Int, Int, Int)
+bsToIntTuple3 = arrayToTuple3 . bsToIntList
+
+bsToIntMatrix :: ByteString -> [[Int]]
+bsToIntMatrix = fmap bsToIntList . BS.lines
+
+bsToIntTuples2 :: ByteString -> [(Int, Int)]
+bsToIntTuples2 = fmap (arrayToTuple2 . bsToIntList) . BS.lines
+
+bsToIntTuples3 :: ByteString -> [(Int, Int, Int)]
+bsToIntTuples3 = fmap (arrayToTuple3 . bsToIntList) . BS.lines
+
+-- IO 出力系
+printYesNo :: Bool -> IO ()
+printYesNo = putStrLn . boolToYesNo
+
+printArrayWithSpace :: (Show a) => [a] -> IO ()
+printArrayWithSpace = putStrLn . unwords . fmap show
+
+printArrayWithLn :: (Show a) => [a] -> IO ()
+printArrayWithLn = putStr . unlines . fmap show
+
+-- IO 入力系
+getLineToInt :: IO Int
+getLineToInt = bsToInt <$> BS.getLine
+
+getLineToIntArray :: IO [Int]
+getLineToIntArray = bsToIntList <$> BS.getLine
+
+getLineToIntTuple2 :: IO (Int, Int)
+getLineToIntTuple2 = bsToIntTuple2 <$> BS.getLine
+
+getLineToIntTuple3 :: IO (Int, Int, Int)
+getLineToIntTuple3 = bsToIntTuple3 <$> BS.getLine
+
+getContentsToIntMatrix :: IO [[Int]]
+getContentsToIntMatrix = bsToIntMatrix <$> BS.getContents
+
+getContentsToIntTuples2 :: IO [(Int, Int)]
+getContentsToIntTuples2 = bsToIntTuples2 <$> BS.getContents
+
+getContentsToIntTuples3 :: IO [(Int, Int, Int)]
+getContentsToIntTuples3 = bsToIntTuples3 <$> BS.getContents
+
+-- デバッグ用
+#ifndef ATCODER
+
+debugProxy :: (Show a) => a -> a
+debugProxy value =
+  let !_ = debug "[DebugProxy]" value
+   in value
+
+debug :: (Show a) => String -> a -> ()
+debug key value = trace (key ++ " : " ++ show value) ()
+
+#else
+
+debugProxy :: (Show a) => a -> a
+debugProxy = id
+
+debug :: (Show a) => String -> a -> ()
+debug _ _ = ()
+
+#endif
+
+-- 便利関数系
+-- 素数判定
+isPrime :: Int -> Bool
+isPrime n
+  | n <= 2 = True
+  | otherwise =
+    let max = ceiling . sqrt $ int2Float n
+     in null [i | i <- [2, 3 .. max], n `mod` i == 0]
+
+-- 約数列挙
+enumerateDivisor :: Int -> [Int]
+enumerateDivisor n = do
+  let max = ceiling . sqrt $ int2Float n
+  toList . fromList $ concat [[x, y] | x <- [1 .. max], n `mod` x == 0, let y = n `div` x]
+
+-- nCr は 組み合わせ (combination)　の計算
+nCr :: Int -> Int -> Int
+nCr n r =
+  let numerator = product $ take r [n, n -1 ..]
+      denominator = product $ take r [1 ..]
+   in numerator `div` denominator
