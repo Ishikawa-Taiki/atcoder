8c989f5d6e11013687c23d3149ad230e6431d34b
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-02-08 21:39:29 +0900

    WA https://atcoder.jp/contests/abc332/submissions/50100460

---
+++ hs/abc332/b/Main.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE CPP #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# HLINT ignore "Redundant flip" #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas -Wno-incomplete-patterns -Wno-unused-imports -Wno-unused-top-binds -Wno-name-shadowing -Wno-unused-matches #-}
+
+-- © 2024 Ishikawa-Taiki
+module Main (main) where
+
+import Control.Monad.Fix (fix)
+import Data.Bool (bool)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Maybe (fromJust)
+import Debug.Trace (trace)
+
+main :: IO ()
+main = do
+  (k, g, m) <- getLineToIntTuple3
+  printArrayWithSpace $ solve k g m
+
+solve :: Int -> Int -> Int -> [Int]
+solve k g m =
+  let (gResult, mResult) = flip fix ((0, 0), 5) \loop ((grass, mag), n) ->
+        if n == 0
+          then (grass, mag)
+          else
+            if grass == g
+              then loop ((0, mag), n - 1) -- グラスが満タンならグラスの水を捨てる
+              else
+                if mag == 0
+                  then loop ((grass, m), n - 1) -- マグカップが空ならマグカップを満タンにする
+                  else do
+                    let fillGrass = g - grass
+                        toGrass = if fillGrass < mag then g else grass + mag
+                        toMag = if fillGrass < mag then mag - fillGrass else 0
+                    loop ((toGrass, toMag), n - 1) -- マグカップからグラスへ水を移す処理
+   in [gResult, mResult]
+
+{- Library -}
+-- データ変換共通
+boolToYesNo :: Bool -> String
+boolToYesNo = bool "No" "Yes"
+
+fst3 :: (a, b, c) -> a
+fst3 (a, _, _) = a
+
+snd3 :: (a, b, c) -> b
+snd3 (_, b, _) = b
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, c) = c
+
+arrayToTuple2 :: [a] -> (a, a)
+arrayToTuple2 (a : b : _) = (a, b)
+
+arrayToTuple3 :: [a] -> (a, a, a)
+arrayToTuple3 (a : b : c : _) = (a, b, c)
+
+bsToInt :: ByteString -> Int
+bsToInt = fst . fromJust . BS.readInt
+
+bsToIntList :: ByteString -> [Int]
+bsToIntList = fmap bsToInt . BS.words
+
+bsToIntTuple2 :: ByteString -> (Int, Int)
+bsToIntTuple2 = arrayToTuple2 . bsToIntList
+
+bsToIntTuple3 :: ByteString -> (Int, Int, Int)
+bsToIntTuple3 = arrayToTuple3 . bsToIntList
+
+bsToIntMatrix :: ByteString -> [[Int]]
+bsToIntMatrix = fmap bsToIntList . BS.lines
+
+bsToIntTuples2 :: ByteString -> [(Int, Int)]
+bsToIntTuples2 = fmap (arrayToTuple2 . bsToIntList) . BS.lines
+
+bsToIntTuples3 :: ByteString -> [(Int, Int, Int)]
+bsToIntTuples3 = fmap (arrayToTuple3 . bsToIntList) . BS.lines
+
+-- IO 出力系
+printYesNo :: Bool -> IO ()
+printYesNo = putStrLn . boolToYesNo
+
+printArrayWithSpace :: (Show a) => [a] -> IO ()
+printArrayWithSpace = putStrLn . unwords . fmap show
+
+printArrayWithLn :: (Show a) => [a] -> IO ()
+printArrayWithLn = putStr . unlines . fmap show
+
+-- IO 入力系
+getLineToString :: IO String
+getLineToString = BS.unpack <$> BS.getLine
+
+getLineToInt :: IO Int
+getLineToInt = bsToInt <$> BS.getLine
+
+getLineToIntArray :: IO [Int]
+getLineToIntArray = bsToIntList <$> BS.getLine
+
+getLineToIntTuple2 :: IO (Int, Int)
+getLineToIntTuple2 = bsToIntTuple2 <$> BS.getLine
+
+getLineToIntTuple3 :: IO (Int, Int, Int)
+getLineToIntTuple3 = bsToIntTuple3 <$> BS.getLine
+
+getContentsToIntMatrix :: IO [[Int]]
+getContentsToIntMatrix = bsToIntMatrix <$> BS.getContents
+
+getContentsToIntTuples2 :: IO [(Int, Int)]
+getContentsToIntTuples2 = bsToIntTuples2 <$> BS.getContents
+
+getContentsToIntTuples3 :: IO [(Int, Int, Int)]
+getContentsToIntTuples3 = bsToIntTuples3 <$> BS.getContents
+
+-- デバッグ用
+#ifndef ATCODER
+
+debugProxy :: (Show a) => a -> a
+debugProxy value =
+  let !_ = debug "[DebugProxy]" value
+   in value
+
+debug :: (Show a) => String -> a -> ()
+debug key value = trace (key ++ " : " ++ show value) ()
+
+#else
+
+debugProxy :: (Show a) => a -> a
+debugProxy = id
+
+debug :: (Show a) => String -> a -> ()
+debug _ _ = ()
+
+#endif
