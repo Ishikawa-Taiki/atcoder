f143884a8a1228592889304aff46b5e3ca483347
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-02-27 01:08:25 +0000

    acc config.json はgit管理＆リンクせず、セットアップ処理中で必要な値を入れることにする

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@
 CUSTOM_SETTINGS_DIRECTORY=$PWD/atcoder-cli
 APP_SETTINGS_DIRECTORY=`acc config-dir`
 
-# 共通の設定ファイルをatcoder-cliの設定フォルダ配下へリンクする
-ln -sf "$CUSTOM_SETTINGS_DIRECTORY/config.json"		"$APP_SETTINGS_DIRECTORY/config.json"
-
 # 本ディレクトリ配下の全てのディレクトリをatcoder-cliの設定フォルダ配下へリンクする
 for item in `ls -d */ | cut -d' ' -f 1`; do
 	ln -nsf "${CUSTOM_SETTINGS_DIRECTORY}/${item%/}"		"${APP_SETTINGS_DIRECTORY}/${item%/}"
 done
+
+# 共通の設定ファイル
+# 環境固有の情報を持っているので、atcoder-cliの設定フォルダ配下へ直接リンクするのではなく、個別で値設定を行う
+# ln -sf "$CUSTOM_SETTINGS_DIRECTORY/config.json"		"$APP_SETTINGS_DIRECTORY/config.json"
+acc config default-test-dirname-format test
