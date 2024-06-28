3817bc076290bb3b0e9b9fea8bd60d316faa9b0a
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-06-28 23:34:57 +0900

    AC https://atcoder.jp/contests/adt_easy_20240529_2/submissions/54996555

---
+++ hs/adt_easy_20240529_2/a/Main.hs
@@ -0,0 +1,149 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# HLINT ignore "Redundant flip" #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas -Wno-incomplete-patterns -Wno-unused-imports -Wno-unused-top-binds -Wno-name-shadowing -Wno-unused-matches #-}
+
+-- © 2024 Ishikawa-Taiki
+module Main (main) where
+
+import Data.Bool (bool)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Maybe (fromJust)
+import Debug.Trace (trace)
+
+main :: IO ()
+main = do
+  x <- getLineToInt
+  putStrLn $ solve x
+
+solve :: Int -> String
+solve x
+  | x <= 9 = "AGC00" ++ show x
+  | x <= 41 = "AGC0" ++ show x
+  | x <= 99 = "AGC0" ++ show (x + 1)
+  | otherwise = "AGC" ++ show x
+
+{- Library -}
+-- データ変換共通
+boolToYesNo :: Bool -> String
+boolToYesNo = bool "No" "Yes"
+
+fst3 :: (a, b, c) -> a
+fst3 (a, _, _) = a
+
+snd3 :: (a, b, c) -> b
+snd3 (_, b, _) = b
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, c) = c
+
+arrayToTuple2 :: [a] -> (a, a)
+arrayToTuple2 (a : b : _) = (a, b)
+
+arrayToTuple3 :: [a] -> (a, a, a)
+arrayToTuple3 (a : b : c : _) = (a, b, c)
+
+tuple2ToArray :: (a, a) -> [a]
+tuple2ToArray (a, b) = [a, b]
+
+tuple3ToArray :: (a, a, a) -> [a]
+tuple3ToArray (a, b, c) = [a, b, c]
+
+bsToInt :: ByteString -> Int
+bsToInt = fst . fromJust . BS.readInt
+
+bsToIntList :: ByteString -> [Int]
+bsToIntList = fmap bsToInt . BS.words
+
+bsToIntTuple2 :: ByteString -> (Int, Int)
+bsToIntTuple2 = arrayToTuple2 . bsToIntList
+
+bsToIntTuple3 :: ByteString -> (Int, Int, Int)
+bsToIntTuple3 = arrayToTuple3 . bsToIntList
+
+bsToIntMatrix :: ByteString -> [[Int]]
+bsToIntMatrix = fmap bsToIntList . BS.lines
+
+bsToIntTuples2 :: ByteString -> [(Int, Int)]
+bsToIntTuples2 = fmap (arrayToTuple2 . bsToIntList) . BS.lines
+
+bsToIntTuples3 :: ByteString -> [(Int, Int, Int)]
+bsToIntTuples3 = fmap (arrayToTuple3 . bsToIntList) . BS.lines
+
+bsToInteger :: ByteString -> Integer
+bsToInteger = fst . fromJust . BS.readInteger
+
+bsToIntegerList :: ByteString -> [Integer]
+bsToIntegerList = fmap bsToInteger . BS.words
+
+-- IO 出力系
+printYesNo :: Bool -> IO ()
+printYesNo = putStrLn . boolToYesNo
+
+printArrayWithSpace :: (Show a) => [a] -> IO ()
+printArrayWithSpace = putStrLn . unwords . fmap show
+
+printArrayWithLn :: (Show a) => [a] -> IO ()
+printArrayWithLn = putStr . unlines . fmap show
+
+printMatrix :: (Show a) => [[a]] -> IO ()
+printMatrix mtx = putStr . unlines $ unwords . fmap show <$> mtx
+
+-- IO 入力系
+getLineToString :: IO String
+getLineToString = BS.unpack <$> BS.getLine
+
+getLineToInt :: IO Int
+getLineToInt = bsToInt <$> BS.getLine
+
+getLineToIntArray :: IO [Int]
+getLineToIntArray = bsToIntList <$> BS.getLine
+
+getLineToIntTuple2 :: IO (Int, Int)
+getLineToIntTuple2 = bsToIntTuple2 <$> BS.getLine
+
+getLineToIntTuple3 :: IO (Int, Int, Int)
+getLineToIntTuple3 = bsToIntTuple3 <$> BS.getLine
+
+getLineToInteger :: IO Integer
+getLineToInteger = bsToInteger <$> BS.getLine
+
+getLineToIntegerArray :: IO [Integer]
+getLineToIntegerArray = bsToIntegerList <$> BS.getLine
+
+getContentsToStringArray :: IO [String]
+getContentsToStringArray = fmap BS.unpack . BS.lines <$> BS.getContents
+
+getContentsToIntMatrix :: IO [[Int]]
+getContentsToIntMatrix = bsToIntMatrix <$> BS.getContents
+
+getContentsToIntTuples2 :: IO [(Int, Int)]
+getContentsToIntTuples2 = bsToIntTuples2 <$> BS.getContents
+
+getContentsToIntTuples3 :: IO [(Int, Int, Int)]
+getContentsToIntTuples3 = bsToIntTuples3 <$> BS.getContents
+
+-- デバッグ用
+#ifndef ATCODER
+
+debugProxy :: (Show a) => a -> a
+debugProxy value =
+  let !_ = debug "[DebugProxy]" value
+   in value
+
+debug :: (Show a) => String -> a -> ()
+debug key value = trace (key ++ " : " ++ show value) ()
+
+#else
+
+debugProxy :: (Show a) => a -> a
+debugProxy = id
+
+debug :: (Show a) => String -> a -> ()
+debug _ _ = ()
+
+#endif
