c8379a5fed381a28486fcd21385e7ece277f5d03
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-07-27 22:00:24 +0900

    AC https://atcoder.jp/contests/abc364/submissions/56041258

---
+++ hs/abc364/b/Main.hs
@@ -0,0 +1,174 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# HLINT ignore "Redundant flip" #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas -Wno-incomplete-patterns -Wno-unused-imports -Wno-unused-top-binds -Wno-name-shadowing -Wno-unused-matches #-}
+
+-- © 2024 Ishikawa-Taiki
+module Main (main) where
+
+import Control.Monad (replicateM)
+import Data.Bool (bool)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Maybe (fromJust)
+import Debug.Trace (trace)
+
+main :: IO ()
+main = do
+  hw@(h, w) <- getLineToIntTuple2
+  current@(i, j) <- getLineToIntTuple2
+  xs <- replicateM h getLineToString
+  x <- getLineToString
+  let (resultX, resultY) = solve xs hw current x
+  printArrayWithSpace [resultX, resultY]
+
+type Size = (Int, Int)
+
+type Point = (Int, Int)
+
+solve :: [String] -> Size -> Point -> String -> Point
+solve _ _ current@(i, j) [] = debugProxy current
+solve matrix hw@(h, w) current@(i, j) (c : command) =
+  let left = if j == 1 || (getMat i (j -1) == '#') then j else j - 1
+      right = if j == w || (getMat i (j + 1) == '#') then j else j + 1
+      up = if i == 1 || (getMat (i -1) j == '#') then i else i - 1
+      down = if i == h || (getMat (i + 1) j == '#') then i else i + 1
+      newI =
+        case c of
+          'U' -> up
+          'D' -> down
+          _ -> i
+      newJ =
+        case c of
+          'L' -> left
+          'R' -> right
+          _ -> j
+      !_ = debugProxy c
+   in solve matrix hw (debugProxy (newI, newJ)) command
+  where
+    getMat :: Int -> Int -> Char
+    getMat y x = (matrix !! (y -1)) !! (x - 1)
+
+{- Library -}
+-- データ変換共通
+boolToYesNo :: Bool -> String
+boolToYesNo = bool "No" "Yes"
+
+fst3 :: (a, b, c) -> a
+fst3 (a, _, _) = a
+
+snd3 :: (a, b, c) -> b
+snd3 (_, b, _) = b
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, c) = c
+
+arrayToTuple2 :: [a] -> (a, a)
+arrayToTuple2 (a : b : _) = (a, b)
+
+arrayToTuple3 :: [a] -> (a, a, a)
+arrayToTuple3 (a : b : c : _) = (a, b, c)
+
+tuple2ToArray :: (a, a) -> [a]
+tuple2ToArray (a, b) = [a, b]
+
+tuple3ToArray :: (a, a, a) -> [a]
+tuple3ToArray (a, b, c) = [a, b, c]
+
+bsToInt :: ByteString -> Int
+bsToInt = fst . fromJust . BS.readInt
+
+bsToIntList :: ByteString -> [Int]
+bsToIntList = fmap bsToInt . BS.words
+
+bsToIntTuple2 :: ByteString -> (Int, Int)
+bsToIntTuple2 = arrayToTuple2 . bsToIntList
+
+bsToIntTuple3 :: ByteString -> (Int, Int, Int)
+bsToIntTuple3 = arrayToTuple3 . bsToIntList
+
+bsToIntMatrix :: ByteString -> [[Int]]
+bsToIntMatrix = fmap bsToIntList . BS.lines
+
+bsToIntTuples2 :: ByteString -> [(Int, Int)]
+bsToIntTuples2 = fmap (arrayToTuple2 . bsToIntList) . BS.lines
+
+bsToIntTuples3 :: ByteString -> [(Int, Int, Int)]
+bsToIntTuples3 = fmap (arrayToTuple3 . bsToIntList) . BS.lines
+
+bsToInteger :: ByteString -> Integer
+bsToInteger = fst . fromJust . BS.readInteger
+
+bsToIntegerList :: ByteString -> [Integer]
+bsToIntegerList = fmap bsToInteger . BS.words
+
+-- IO 出力系
+printYesNo :: Bool -> IO ()
+printYesNo = putStrLn . boolToYesNo
+
+printArrayWithSpace :: (Show a) => [a] -> IO ()
+printArrayWithSpace = putStrLn . unwords . fmap show
+
+printArrayWithLn :: (Show a) => [a] -> IO ()
+printArrayWithLn = putStr . unlines . fmap show
+
+printMatrix :: (Show a) => [[a]] -> IO ()
+printMatrix mtx = putStr . unlines $ unwords . fmap show <$> mtx
+
+-- IO 入力系
+getLineToString :: IO String
+getLineToString = BS.unpack <$> BS.getLine
+
+getLineToInt :: IO Int
+getLineToInt = bsToInt <$> BS.getLine
+
+getLineToIntArray :: IO [Int]
+getLineToIntArray = bsToIntList <$> BS.getLine
+
+getLineToIntTuple2 :: IO (Int, Int)
+getLineToIntTuple2 = bsToIntTuple2 <$> BS.getLine
+
+getLineToIntTuple3 :: IO (Int, Int, Int)
+getLineToIntTuple3 = bsToIntTuple3 <$> BS.getLine
+
+getLineToInteger :: IO Integer
+getLineToInteger = bsToInteger <$> BS.getLine
+
+getLineToIntegerArray :: IO [Integer]
+getLineToIntegerArray = bsToIntegerList <$> BS.getLine
+
+getContentsToStringArray :: IO [String]
+getContentsToStringArray = fmap BS.unpack . BS.lines <$> BS.getContents
+
+getContentsToIntMatrix :: IO [[Int]]
+getContentsToIntMatrix = bsToIntMatrix <$> BS.getContents
+
+getContentsToIntTuples2 :: IO [(Int, Int)]
+getContentsToIntTuples2 = bsToIntTuples2 <$> BS.getContents
+
+getContentsToIntTuples3 :: IO [(Int, Int, Int)]
+getContentsToIntTuples3 = bsToIntTuples3 <$> BS.getContents
+
+-- デバッグ用
+#ifndef ATCODER
+
+debugProxy :: (Show a) => a -> a
+debugProxy value =
+  let !_ = debug "[DebugProxy]" value
+   in value
+
+debug :: (Show a) => String -> a -> ()
+debug key value = trace (key ++ " : " ++ show value) ()
+
+#else
+
+debugProxy :: (Show a) => a -> a
+debugProxy = id
+
+debug :: (Show a) => String -> a -> ()
+debug _ _ = ()
+
+#endif
