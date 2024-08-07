7cd6f2da7086562646c4e97606ee72aff6614cff
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-08-08 01:09:58 +0900

    提出：

--- hs/abc365/c/Main.hs
+++ hs/abc365/c/Main.hs
@@ -22,18 +22,29 @@ main = do
   xs <- getLineToIntegerArray
   putStrLn $ maybe "infinite" show (solve xs n m)
 
--- 合計がM円以下なら無限に出来る
--- そうでないデータはある境界で綺麗に分かれるので、二分探索を使うとよい
+-- 問題概略
+--  交通費のリストと予算が与えられる
+--  合計予算を超えないように交通費補助額の上限を設定したいが、最大いくらにできるか？
+--  上限額を無限に大きくできる場合、そのことを報告する
+-- 戦略
+--  交通費の合計額が予算より小さい場合、全て支払ってあげて問題ないので無限を報告する
+--  そうでない場合はOKとなる最大値を見つけなければならないが、リスト内の分布が分からないので実際に値を入れながら試した方が良さそう
+--  リストの要素数が結構大きいので、実際に入れる値を探す処理はある程度効率よく見つけたいところ
+--  問題の特性より予算の閾値には明確なボーダーライン(単調性)があるため、二分探索を用いることで効率よく間引く事が出来る
+--  二分探索をする際は絶対にOK/NGになる値を初期値にする必要があるので、
+--  　予算0(支払わない)をOKの初期値、予算m+1(予算より大きな値まで払う)をNGの初期値にする
+--  計算量的には　最初に合計を取るO(N) + ボーダーラインを探る二分探索O(logN) * ボーダーラインを探る過程での全要素のお試し計算O(N) あたりなので、問題ない見込みとなる
 solve :: [Integer] -> Integer -> Integer -> Maybe Integer
 solve xs n m =
   if sum xs <= m
     then Nothing
     else Just . fst . binarySearch (not . isOver xs m) $ (0, m + 1)
 
--- 支払いリストに経費を設定した時、予算超過になるかどうか
+-- 支払いリストに予算と経費を設定した際、予算超過になるかどうかを確認する
 isOver :: [Integer] -> Integer -> Integer -> Bool
 isOver list budget expenses = budget < sum (min expenses <$> list)
 
+-- 値が有効化どうかを確認する関数と現在のOK/NG範囲を受け取り、最終的なOK/NG範囲を返却する
 binarySearch :: (Integer -> Bool) -> (Integer, Integer) -> (Integer, Integer)
 binarySearch check (ok, ng)
   | abs (ng - ok) == 1 = (ok, ng)
