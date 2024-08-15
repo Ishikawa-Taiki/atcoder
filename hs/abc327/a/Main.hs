71711b8b724c05417abedc17a75e71571bfa7bcd
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-08-15 01:05:58 +0000

    提出：https://atcoder.jp/contests/abc327/submissions/56701098

--- hs/abc327/a/Main.hs
+++ hs/abc327/a/Main.hs
@@ -12,17 +12,18 @@ module Main (main) where
 import Data.Bool (bool)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import Data.List (isInfixOf)
 import Data.Maybe (fromJust)
 import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntArray
-  print $ solve xs
+  _ <- getLineToInt
+  xs <- getLineToString
+  printYesNo $ solve xs
 
-solve :: [Int] -> Int
-solve xs = undefined
+solve :: [Char] -> Bool
+solve xs = isInfixOf "ab" xs || isInfixOf "ba" xs
 
 {- Library -}
 -- データ変換共通
