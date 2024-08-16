20a03e07775782f36d0386daa1261dda3a3375c5
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-08-16 15:44:17 +0900

    提出：https://atcoder.jp/contests/abc335/submissions/56736118

--- hs/abc335/c/Main.hs
+++ hs/abc335/c/Main.hs
@@ -1,41 +1,175 @@
-#!/usr/bin/env runghc
--- © 2024 Ishikawa-Taiki
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TupleSections #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# HLINT ignore "Redundant flip" #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas -Wno-incomplete-patterns -Wno-unused-imports -Wno-unused-top-binds -Wno-name-shadowing -Wno-unused-matches #-}
 
+-- © 2024 Ishikawa-Taiki
 module Main (main) where
-import qualified Data.Bifunctor
+
+import Control.Monad (replicateM)
+import Data.Bifunctor (Bifunctor (bimap, first, second))
+import Data.Bool (bool)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Maybe (fromJust)
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  [n, q] <- map read . words <$> getLine :: IO [Int]
-  -- 複数行、複数列の入力の受け取り
-  querys <- map words . lines <$> getContents
-  let result = solve n q querys
-  -- 複数行の出力
-  putStr $ unlines result
+  (n, q) <- getLineToIntTuple2
+  query <- replicateM q getLineToString
+  putStrLn . unlines $ solve n query
 
-solve :: Int -> Int -> [[String]] -> [String]
-solve n q querys =
-  let history = reverse [(v,0) | v <- [1..n]] -- 計算量削減のため、最新の操作は後ろに追加していく
-  in fst $ foldl execCommand ([], history) querys
+type Point = (Int, Int)
 
-execCommand :: ([String], [(Int, Int)]) -> [String] -> ([String], [(Int, Int)])
-execCommand current (c1:c2:_) = createCommand c1 c2 current
+type AccResult = ([Point], [String])
 
--- コマンドと最新状況を受け取って、実行結果を返す
-createCommand :: String -> String -> ([String], [(Int, Int)]) -> ([String], [(Int, Int)])
-createCommand "1" "U" current = Data.Bifunctor.second (move (0, 1)) current
-createCommand "1" "D" current = Data.Bifunctor.second (move (0, -1)) current
-createCommand "1" "R" current = Data.Bifunctor.second (move (1, 0)) current
-createCommand "1" "L" current = Data.Bifunctor.second (move (-1, 0)) current
-createCommand "2" c2  current = Data.Bifunctor.first (++ [toString (read c2::Int) (snd current)]) current
+solve :: Int -> [String] -> [String]
+solve n = reverse . snd . foldl acc ((,0) <$> [1 .. n], [])
+  where
+    acc :: AccResult -> String -> AccResult
+    acc (history, output) query =
+      let (c : v : _) = words query
+          command = read c :: Int
+          direction = head v
+          parts = read v :: Int
+          current = head history
+       in if command == 1
+            then (move direction current : history, output)
+            else (history, showPoint (history !! (parts - 1)) : output)
+      where
+        showPoint :: Point -> String
+        showPoint (x, y) = show x ++ " " ++ show y
 
--- x y の操作量と履歴を受け取って、履歴に操作結果を追加する
-move :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
-move (x, y) h = h ++ [Data.Bifunctor.bimap (+x) (+y) (last h)]
+move :: Char -> Point -> Point
+move 'U' = second (+ 1)
+move 'D' = second (`subtract` 1)
+move 'L' = first (+ 1)
+move 'R' = first (`subtract` 1)
+move _ = id
 
--- 指定パーツ位置と履歴を受け取って、該当パーツ位置の文字列表現を返却する
-toString :: Int -> [(Int, Int)] -> String
-toString n h =
-  let part = h !! (length h - n)
-  in show (fst part) ++ " " ++ show (snd part)
+{- Library -}
+-- データ変換共通
+boolToYesNo :: Bool -> String
+boolToYesNo = bool "No" "Yes"
+
+fst3 :: (a, b, c) -> a
+fst3 (a, _, _) = a
+
+snd3 :: (a, b, c) -> b
+snd3 (_, b, _) = b
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, c) = c
+
+arrayToTuple2 :: [a] -> (a, a)
+arrayToTuple2 (a : b : _) = (a, b)
+
+arrayToTuple3 :: [a] -> (a, a, a)
+arrayToTuple3 (a : b : c : _) = (a, b, c)
+
+tuple2ToArray :: (a, a) -> [a]
+tuple2ToArray (a, b) = [a, b]
+
+tuple3ToArray :: (a, a, a) -> [a]
+tuple3ToArray (a, b, c) = [a, b, c]
+
+bsToInt :: ByteString -> Int
+bsToInt = fst . fromJust . BS.readInt
+
+bsToIntList :: ByteString -> [Int]
+bsToIntList = fmap bsToInt . BS.words
+
+bsToIntTuple2 :: ByteString -> (Int, Int)
+bsToIntTuple2 = arrayToTuple2 . bsToIntList
+
+bsToIntTuple3 :: ByteString -> (Int, Int, Int)
+bsToIntTuple3 = arrayToTuple3 . bsToIntList
+
+bsToIntMatrix :: ByteString -> [[Int]]
+bsToIntMatrix = fmap bsToIntList . BS.lines
+
+bsToIntTuples2 :: ByteString -> [(Int, Int)]
+bsToIntTuples2 = fmap (arrayToTuple2 . bsToIntList) . BS.lines
+
+bsToIntTuples3 :: ByteString -> [(Int, Int, Int)]
+bsToIntTuples3 = fmap (arrayToTuple3 . bsToIntList) . BS.lines
+
+bsToInteger :: ByteString -> Integer
+bsToInteger = fst . fromJust . BS.readInteger
+
+bsToIntegerList :: ByteString -> [Integer]
+bsToIntegerList = fmap bsToInteger . BS.words
+
+-- IO 出力系
+printYesNo :: Bool -> IO ()
+printYesNo = putStrLn . boolToYesNo
+
+printArrayWithSpace :: (Show a) => [a] -> IO ()
+printArrayWithSpace = putStrLn . unwords . fmap show
+
+printArrayWithLn :: (Show a) => [a] -> IO ()
+printArrayWithLn = putStr . unlines . fmap show
+
+printMatrix :: (Show a) => [[a]] -> IO ()
+printMatrix mtx = putStr . unlines $ unwords . fmap show <$> mtx
+
+-- IO 入力系
+getLineToString :: IO String
+getLineToString = BS.unpack <$> BS.getLine
+
+getLineToInt :: IO Int
+getLineToInt = bsToInt <$> BS.getLine
+
+getLineToIntArray :: IO [Int]
+getLineToIntArray = bsToIntList <$> BS.getLine
+
+getLineToIntTuple2 :: IO (Int, Int)
+getLineToIntTuple2 = bsToIntTuple2 <$> BS.getLine
+
+getLineToIntTuple3 :: IO (Int, Int, Int)
+getLineToIntTuple3 = bsToIntTuple3 <$> BS.getLine
+
+getLineToInteger :: IO Integer
+getLineToInteger = bsToInteger <$> BS.getLine
+
+getLineToIntegerArray :: IO [Integer]
+getLineToIntegerArray = bsToIntegerList <$> BS.getLine
+
+getContentsToStringArray :: IO [String]
+getContentsToStringArray = fmap BS.unpack . BS.lines <$> BS.getContents
+
+getContentsToIntMatrix :: IO [[Int]]
+getContentsToIntMatrix = bsToIntMatrix <$> BS.getContents
+
+getContentsToIntTuples2 :: IO [(Int, Int)]
+getContentsToIntTuples2 = bsToIntTuples2 <$> BS.getContents
+
+getContentsToIntTuples3 :: IO [(Int, Int, Int)]
+getContentsToIntTuples3 = bsToIntTuples3 <$> BS.getContents
+
+-- デバッグ用
+#ifndef ATCODER
+
+debugProxy :: (Show a) => a -> a
+debugProxy value =
+  let !_ = debug "[DebugProxy]" value
+   in value
+
+debug :: (Show a) => String -> a -> ()
+debug key value = trace (key ++ " : " ++ show value) ()
+
+#else
+
+debugProxy :: (Show a) => a -> a
+debugProxy = id
+
+debug :: (Show a) => String -> a -> ()
+debug _ _ = ()
+
+#endif
