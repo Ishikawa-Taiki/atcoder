3bdc7f2266ccbfadccf6dcc8037b9f3e8205e057
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-08-28 13:01:08 +0900

    提出：https://atcoder.jp/contests/abc323/submissions/57193336

--- hs/abc323/c/Main.hs
+++ hs/abc323/c/Main.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
 {-# HLINT ignore "Unused LANGUAGE pragma" #-}
 {-# HLINT ignore "Redundant flip" #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas -Wno-incomplete-patterns -Wno-unused-imports -Wno-unused-top-binds -Wno-name-shadowing -Wno-unused-matches #-}
@@ -9,20 +11,62 @@
 -- © 2024 Ishikawa-Taiki
 module Main (main) where
 
+import Data.Array.Unboxed (UArray, listArray, (!))
 import Data.Bool (bool)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import Data.List (minimumBy, sortBy)
 import Data.Maybe (fromJust)
+import Data.Monoid (Sum (Sum, getSum))
 import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntArray
-  print $ solve xs
+  (n, m) <- getLineToIntTuple2
+  as <- getLineToIntArray
+  s <- getContentsToStringArray
+  printArrayWithLn $ solve as s n m
 
-solve :: [Int] -> Int
-solve xs = undefined
+{-
+問題概要：
+ 問題の配点リストと、プレイヤー毎の現在の正解状況リストが与えられる
+ 各プレイヤーについて、あと難問解くことで他の全てのプレイヤーを超えられるかを求めよ
+ なお各プレイヤー番号がボーナス点として付与されているので、答えは一意に定まる
+戦略：
+ 自分以外の一番点数が高い人を越えればよい
+ そのために未正解の項目のうち配点が高いものから答えると難問で正解になるかを考えれば良さそう
+-}
+
+solve :: [Int] -> [String] -> Int -> Int -> [Int]
+solve as xxs n m =
+  let points = listArray @UArray (1, m) as
+      currentScores =
+        ( \(xs, i) ->
+            let status = sortBy (flip compare) $ zipWith (\x no -> (points ! no, x == 'o')) xs [1 .. m]
+             in (,status) . (+ i) . sumIf snd fst $ status
+        )
+          <$> zip xxs [1 .. n]
+      topScore = maximum currentScores
+      needs =
+        (\(score, status) -> calcCount (fst topScore - score) status)
+          <$> currentScores
+   in needs
+  where
+    calcCount :: Int -> [Status] -> Int
+    calcCount needsPoint status = snd $ foldl (f needsPoint) (0, 0) status
+      where
+        f :: Int -> (Int, Int) -> Status -> (Int, Int)
+        f needsPoint result@(currentSum, count) (point, isCorrect)
+          | needsPoint <= currentSum = result
+          | isCorrect = result
+          | otherwise = (currentSum + point, succ count)
+
+type Status = (Int, Bool)
+
+type Score = (Int, [Status])
+
+sumIf :: (a -> Bool) -> (a -> Int) -> [a] -> Int
+sumIf f g = getSum . foldMap (\v -> bool (Sum 0) (Sum $ g v) $ f v)
 
 {- Library -}
 -- データ変換共通
