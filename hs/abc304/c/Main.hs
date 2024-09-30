7269e5ebfa97d84e70e691ff77924d7e6fbcb646
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-09-30 23:49:01 +0900

    提出：https://atcoder.jp/contests/abc304/submissions/58299988

--- hs/abc304/c/Main.hs
+++ hs/abc304/c/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TypeApplications #-}
 {-# HLINT ignore "Unused LANGUAGE pragma" #-}
 {-# HLINT ignore "Redundant flip" #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas -Wno-incomplete-patterns -Wno-unused-imports -Wno-unused-top-binds -Wno-name-shadowing -Wno-unused-matches #-}
@@ -9,6 +10,13 @@
 -- © 2024 Ishikawa-Taiki
 module Main (main) where
 
+import Control.Monad (forM_, when)
+import Control.Monad.Fix (fix)
+import Control.Monad.ST (ST)
+import Data.Array (Array)
+import Data.Array.IArray (elems, listArray, (!))
+import Data.Array.MArray (readArray, writeArray)
+import Data.Array.ST (MArray (newArray), STUArray, runSTUArray)
 import Data.Bool (bool)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
@@ -17,12 +25,46 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  (n, d) <- getLineToIntTuple2
+  xs <- getContentsToIntTuples2
+  putStr . unlines . fmap boolToYesNo $ solve xs n d
 
-solve :: [Int] -> Int
-solve xs = undefined
+{-
+問題概要
+N人の人物のXY座標のリストと、距離Dが与えられる
+ユークリッド距離D以内の人はウイルスに感染する状態で人1がウイルスに感染した
+それぞれの人がウイルスに感染しているかどうかを出力せよ
+
+戦略
+人1からNまでの人が感染しているかどうかを示すリストを用意し、DFSを用いて人1から辿れる人にマークをしていく
+ユークリッド距離の計算に√を用いると小数点誤差が産まれるので、Dを2乗した値を使うことで回避する
+
+-}
+
+solve :: [(Int, Int)] -> Int -> Int -> [Bool]
+solve xs n d =
+  let as = listArray @Array (1, n) xs
+   in elems $
+        runSTUArray $ do
+          seen <- newArray (1, n) False :: ST s (STUArray s Int Bool)
+
+          -- DFS実装に挑戦
+          flip fix 1 \dfs v1 -> do
+            writeArray seen v1 True
+
+            forM_ [1 .. n] $ \v2 -> do
+              v2Seen <- readArray seen v2
+              when (not v2Seen && isConnected d (as ! v1) (as ! v2)) $ dfs v2
+
+          return seen
+
+-- 与えられた2点の座標がユークリッド距離D以内かどうかを返す
+isConnected :: Int -> (Int, Int) -> (Int, Int) -> Bool
+isConnected d v1@(y1, x1) v2@(y2, x2) =
+  let yDiff = (y1 - y2) ^ 2
+      xDiff = (x1 - x2) ^ 2
+      distance = yDiff + xDiff
+   in distance <= d ^ 2
 
 {- Library -}
 -- データ変換共通
