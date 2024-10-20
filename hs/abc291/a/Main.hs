de4e04093babc783d5f360280e10e7275b565e91
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-10-20 15:35:42 +0900

    提出：https://atcoder.jp/contests/abc291/submissions/59006657

--- hs/abc291/a/Main.hs
+++ hs/abc291/a/Main.hs
@@ -13,17 +13,18 @@ module Main (main) where
 import Data.Bool (bool)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import Data.Char (isUpper)
+import Data.List (findIndex)
 import Data.Maybe (fromJust)
 import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
+  xs <- getLineToString
   print $ solve xs
 
-solve :: [Int] -> Int
-solve xs = undefined
+solve :: [Char] -> Int
+solve xs = fromJust $ succ <$> findIndex isUpper xs
 
 {- Library -}
 -- データ変換共通
