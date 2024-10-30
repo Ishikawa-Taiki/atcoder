a0e9508b26499cf8f80696fbbdfa75ebb8f8de9d
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-10-30 12:38:44 +0000

    提出：

--- hs/abc283/a/Main.hs
+++ hs/abc283/a/Main.hs
@@ -19,11 +19,7 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = undefined
+  print $ a ^ b
 
 {- Library -}
 -- データ変換共通
