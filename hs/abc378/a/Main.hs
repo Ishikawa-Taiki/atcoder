faeb70cd0779673aa4e249a7583521b6dbb71f27
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-11-11 00:36:58 +0000

    提出：https://atcoder.jp/contests/abc378/submissions/59672611

--- hs/abc378/a/Main.hs
+++ hs/abc378/a/Main.hs
@@ -13,17 +13,21 @@ module Main (main) where
 import Data.Bool (bool)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import Data.List (group, sort)
 import Data.Maybe (fromJust)
 import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
   print $ solve xs
 
 solve :: [Int] -> Int
-solve xs = undefined
+solve xs = sum $ map ((`div` 2) . snd) $ rle . sort $ xs
+
+-- runLengthEncoding / ランレングス圧縮(リスト上の連続したデータを、データ一つ+連続した長さのリストに変換する)
+rle :: (Eq a) => [a] -> [(a, Int)]
+rle = map (\x -> (head x, length x)) . group
 
 {- Library -}
 -- データ変換共通
