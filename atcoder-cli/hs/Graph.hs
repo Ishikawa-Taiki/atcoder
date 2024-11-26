fbdde8df457f0bf0815f36547dc7174d8675f7e3
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-11-26 11:36:39 +0000

    有効グラフの隣接リスト表現を追加

--- atcoder-cli/hs/Graph.hs
+++ atcoder-cli/hs/Graph.hs
@@ -6,9 +6,14 @@ import Data.Tuple (swap)
 
 {- 無効グラフ -}
 -- 隣接リスト表現
-adjacencyListUndirected :: Ord a => [(a, a)] -> M.Map a [a]
+adjacencyListUndirected :: (Ord a) => [(a, a)] -> M.Map a [a]
 adjacencyListUndirected pairs = M.fromListWith (++) $ concat [[(a, [b]), (b, [a])] | (a, b) <- pairs]
 
 -- 隣接行列表現
 adjacencyMatrixUndirected :: (Ix b, Num b) => [(b, b)] -> b -> UArray (b, b) Bool
 adjacencyMatrixUndirected pairs vCount = accumArray @UArray (||) False ((1, 1), (vCount, vCount)) $ concat [[(pair, True), (swap pair, True)] | pair <- pairs]
+
+{- 有効グラフ -}
+-- 隣接リスト表現
+adjacencyListDirected :: (Ord a) => [(a, a)] -> M.Map a [a]
+adjacencyListDirected pairs = M.fromListWith (++) $ [(a, [b]) | (a, b) <- pairs]
