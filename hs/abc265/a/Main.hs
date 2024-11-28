43764a1618c0db60ad38954038dde4f76057be11
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-11-28 00:32:21 +0000

    提出：https://atcoder.jp/contests/abc265/submissions/60243994

--- hs/abc265/a/Main.hs
+++ hs/abc265/a/Main.hs
@@ -28,15 +28,16 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  (x, y, n) <- getLineToIntTuple3
+  print $ solve x y n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: Int -> Int -> Int -> Int
+solve x y n = result
   where
-    result = undefined
+    allx = x * n
+    (dy, my) = n `divMod` 3
+    containy = dy * y + my * x
+    result = min allx containy
 
 {- Library -}
 -- データ変換共通
