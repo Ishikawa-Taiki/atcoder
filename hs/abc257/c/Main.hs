45bccf3111765c9175cb19308ccfdd249ccf5e32
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-12-08 11:40:29 +0900

    中断

--- hs/abc257/c/Main.hs
+++ hs/abc257/c/Main.hs
@@ -29,14 +29,46 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  s <- getLineToString
+  ws <- getLineToIntList
+  print $ solve ws s n
 
-solve :: [Int] -> Int
-solve xs = result
+{-
+問題概要
+大人or子供と体重の情報の人リストが与えられる
+体重を基準に大人/子供を判定しようとする場合、最大で何人を正しく判定できるか
+
+戦略
+リストを大人と子供に分け、それぞれソートしておく
+大人の値を基準に少しずつ最小値から大きくしていった時、何人の子供が正しく判定できるようになっていくかを求めると良さそう
+候補の数値は大人の値自体(ぎりぎり入る)と、値+1(ぎりぎり外れる)を見ていく
+体重が同じ人が複数いる可能性を考えると、RLEしておけば値の種類と人数を切り離して考えやすい？
+(そうすると、何人増えるか/何人減るかを高速に求めるために、累積の人数が必要そう？)
+
+-}
+
+solve :: [Int] -> String -> Int -> Int
+solve ws s n = result
   where
+    f = rle . sort . map fst
+    !(adult, child) = debugProxy "a,c" $ bimap f f $ partition ((== '1') . snd) $ zip ws s
     result = undefined
+
+-- runLengthEncoding / ランレングス圧縮(リスト上の連続したデータを、データ一つ+連続した長さのリストに変換する)
+rle :: (Eq a) => [a] -> [(a, Int)]
+rle = map (\x -> (head x, length x)) . group
+
+-- 二分探索
+-- 値が有効化どうかを確認する関数と、現在のOK/NG範囲を受け取り、最終的なOK/NG範囲を返却する
+-- (ok, ng は見に行かないので、両端が確定しない場合は1つ外側を指定すると良さそう？)
+binarySearch :: (Int -> Bool) -> (Int, Int) -> (Int, Int)
+binarySearch check (ok, ng)
+  | abs (ng - ok) == 1 = (ok, ng)
+  | otherwise =
+    let mid = (ok + ng) `div` 2
+     in if check mid
+          then binarySearch check (mid, ng)
+          else binarySearch check (ok, mid)
 
 {- Library -}
 -- データ変換共通
