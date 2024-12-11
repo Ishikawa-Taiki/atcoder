cc1927923aeaff7b80cbfcfb110ba7a9c4d154d3
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-12-11 00:25:33 +0000

    提出：https://atcoder.jp/contests/abc254/submissions/60642529

--- hs/abc254/a/Main.hs
+++ hs/abc254/a/Main.hs
@@ -28,10 +28,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getLineToString
+  putStrLn $ drop 1 xs
 
 solve :: [Int] -> Int
 solve xs = result
