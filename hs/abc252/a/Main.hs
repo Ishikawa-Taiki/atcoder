8c5072797983cceed0f64909af293a271d2d2dbe
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-12-14 19:41:58 +0900

    提出：https://atcoder.jp/contests/abc252/submissions/60716089

--- hs/abc252/a/Main.hs
+++ hs/abc252/a/Main.hs
@@ -29,14 +29,8 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  let m = M.fromList $ zip [97 ..] ['a' ..]
+  putChar $ m M.! n
 
 {- Library -}
 -- データ変換共通
