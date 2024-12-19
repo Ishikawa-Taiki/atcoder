43bcbaa7436de8c3f5925d38823ff269bf1faa12
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-12-19 00:27:53 +0000

    提出：https://atcoder.jp/contests/abc248/submissions/60882852

--- hs/abc248/a/Main.hs
+++ hs/abc248/a/Main.hs
@@ -28,15 +28,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  xs <- getLineToString
+  putChar $ head . S.toList . S.difference (S.fromList "0123456789") $ S.fromList xs
 
 {- Library -}
 -- データ変換共通
