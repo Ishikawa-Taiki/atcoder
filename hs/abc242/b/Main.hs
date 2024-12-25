bff732e16ffbe74578b3a38b5c67983d32367561
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2024-12-25 16:59:15 +0900

    提出：https://atcoder.jp/contests/abc242/submissions/61076985

--- hs/abc242/b/Main.hs
+++ hs/abc242/b/Main.hs
@@ -28,15 +28,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  xs <- getLineToString
+  putStrLn $ sort xs
 
 {- Library -}
 -- データ変換共通
