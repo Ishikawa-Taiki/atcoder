ca462d33652de285d868d05b873c2ce66743b66c
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2024-12-27 01:28:24 +0000

    提出：https://atcoder.jp/contests/abc240/submissions/61110048

--- hs/abc240/b/Main.hs
+++ hs/abc240/b/Main.hs
@@ -29,14 +29,8 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  print . S.size . S.fromList $ xs
 
 {- Library -}
 -- データ変換共通
