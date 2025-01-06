c6377794b98dadb9bf009cfa992e9bbadd2bd37c
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-06 23:49:06 +0900

    提出：https://atcoder.jp/contests/abc232/submissions/61444805

--- hs/abc232/a/Main.hs
+++ hs/abc232/a/Main.hs
@@ -28,15 +28,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  (a : x : b : _) <- getLineToString
+  print $ digitToInt a * digitToInt b
 
 {- Library -}
 -- データ変換共通
