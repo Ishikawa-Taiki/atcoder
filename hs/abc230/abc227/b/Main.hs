ee1a643720d6e90742884f1d6e80dfce47eb7a00
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-11 22:22:14 +0900

    提出：https://atcoder.jp/contests/abc227/submissions/61595224

--- hs/abc230/abc227/b/Main.hs
+++ hs/abc230/abc227/b/Main.hs
@@ -29,14 +29,15 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
   print $ solve xs
 
 solve :: [Int] -> Int
 solve xs = result
   where
-    result = undefined
+    candidates = [f i j | i <- [1 .. 1000], j <- [1 .. 1000]]
+    f a b = (4 * a * b) + (3 * a) + (3 * b)
+    result = length $ filter (`notElem` candidates) xs
 
 {- Library -}
 -- データ変換共通
