47a364532defae657869f5f22ac587c1be179486
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-15 20:41:01 +0900

    提出：https://atcoder.jp/contests/abc225/submissions/61708676

--- hs/abc225/a/Main.hs
+++ hs/abc225/a/Main.hs
@@ -28,15 +28,13 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
+  xs <- getLineToString
   print $ solve xs
 
-solve :: [Int] -> Int
+solve :: [Char] -> Int
 solve xs = result
   where
-    result = undefined
+    result = S.size . S.fromList $ permutations xs
 
 {- Library -}
 -- データ変換共通
