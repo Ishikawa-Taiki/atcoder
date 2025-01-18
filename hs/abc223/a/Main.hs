2bb16b24192e6ab938742874a561ae64d523a03f
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-18 19:59:27 +0900

    提出：https://atcoder.jp/contests/abc223/submissions/61776607

--- hs/abc223/a/Main.hs
+++ hs/abc223/a/Main.hs
@@ -28,15 +28,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  x <- getLineToInt
+  printYesNo $ 0 == x `mod` 100 && x /= 0
 
 {- Library -}
 -- データ変換共通
