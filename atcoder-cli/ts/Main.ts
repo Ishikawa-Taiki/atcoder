75ec700038e1ac5ea77c98174bcc74ceb823ee07
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-01-19 13:56:59 +0000

    Typescriptのテンプレートを仮更新

--- atcoder-cli/ts/Main.ts
+++ atcoder-cli/ts/Main.ts
@@ -1,22 +1,39 @@
-// © 2024 Ishikawa-Taiki
 import * as fs from "fs";
 
 function main() {
-  const lines = IOUtil.readAllLines();
+  const head = IO.getLineToIntList();
+  const lines = IO.getContentsToIntMatrix();
+  console.log(head);
   console.log(lines);
 }
 
-export module IOUtil {
-  /**
-   * 全ての標準入力を読み取って返却する
-   * @returns 行毎の配列
-   */
-  export function readAllLines() {
-    return fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+namespace IO {
+  // TODO: 構造を見直す(この制御方法だと入出力で時間がかかりすぎてしまうかもしれない)
+  const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n")
+  export function getLineToString(): string {
+    return input.shift()!;
+  }
+  export function getLineToInt(): number {
+    return +input.shift()!;
+  }
+  export function getLineToIntList(): number[] {
+    // TODO: Haskell踏襲で良い感じに内部関数を分ける
+    return input.shift()!.split(" ").map(Number);
+  }
+  export function getContentsToStringList(): string[] {
+    // TODO: inputを空にする
+    return input;
+  }
+  export function getContentsToIntMatrix(): number[][] {
+    // TODO: inputを空にする
+    return input.map((s) => s.split(" ").map(Number))
+  }
+  export function printYesNo(p: boolean): void {
+    console.log(p ? "Yes" : "No");
   }
 }
 
-export module StringUtil {
+namespace StringUtil {
   /**
    * 文字列を反転する
    * @returns 反転した文字列
