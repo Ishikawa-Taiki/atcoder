e59f21e9f6bc5ea8eb6fee1626bee91937389a7c
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-01-20 01:25:15 +0000

    コンテスト参加

---
+++ ts/abc387/a/Main.ts
@@ -0,0 +1,49 @@
+import * as fs from "fs";
+
+function main() {
+  const head = IO.getLineToIntList();
+  const lines = IO.getContentsToIntMatrix();
+  console.log(head);
+  console.log(lines);
+}
+
+namespace IO {
+  // TODO: Haskell踏襲で良い感じに内部関数を分ける
+  // TODO: 構造を見直す(入出力だけで時間かかりすぎかもしれない)
+  const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n")
+  let index = 0;
+  export function getLineToString(): string {
+    return input[index++];
+  }
+  export function getLineToInt(): number {
+    return +input[index++];
+  }
+  export function getLineToIntList(): number[] {
+    return input[index++].split(" ").map(Number);
+  }
+  export function getContentsToStringList(): string[] {
+    const result = input.slice(index)
+    index = input.length
+    return result;
+  }
+  export function getContentsToIntMatrix(): number[][] {
+    const result = input.slice(index).map((s) => s.split(" ").map(Number));
+    index = input.length
+    return result;
+  }
+  export function printYesNo(p: boolean): void {
+    console.log(p ? "Yes" : "No");
+  }
+}
+
+namespace StringUtil {
+  /**
+   * 文字列を反転する
+   * @returns 反転した文字列
+   */
+  export function reverse(s: string): string {
+    return s.split("").reverse().join("");
+  }
+}
+
+main();
