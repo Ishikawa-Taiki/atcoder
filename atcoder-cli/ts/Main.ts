4a429e1da01cae03b34b15714255089445866c50
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-01-21 01:07:51 +0000

    Typescriptのテンプレートを更新

--- atcoder-cli/ts/Main.ts
+++ atcoder-cli/ts/Main.ts
@@ -42,6 +42,13 @@ namespace Util {
   export const reverse = (s: string) => s.split("").reverse().join("");
   export const range =
     (from: number, to: number) => Array.from({ length: (to - from + 1) }, (_, i) => from + i);
+  export const sum = (array: number[]) => array.reduce((m, v) => m + v, 0);
+  export const max = (array: number[]) => array.reduce((m, v) => Math.max(m, v), 0);
+  export const min = (array: number[]) => array.reduce((m, v) => Math.min(m, v), Infinity);
+  export const sort = (array: number[]) => array.slice().sort((a, b) => a - b);
+  export const countIf = (array: Array<any>, predicate: (v: any) => boolean) => array.filter(predicate).length;
+  export const repeat = (n: number, value: any) => Array.from({ length: n }).fill(value);
+  export const countElements = <T>(array: Array<T>): Map<T, number> => array.reduce((m, v) => m.set(v, m.get(v) ? m.get(v)! + 1 : 1), new Map<T, number>());
 }
 
 main();
