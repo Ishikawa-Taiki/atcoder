8ff125cc41ef6b0b81a4e31b79e7a5c80b8e112d
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-22 00:46:58 +0900

    再挑戦

--- ts/abc388/c/Main-20250122-004658.ts
+++ ts/abc388/c/Main-20250122-004658.ts
@@ -3,8 +3,13 @@ import * as fs from "fs";
 function main() {
   const n = IO.getLineToInt();
   const xs = IO.getLineToIntList();
-  IO.print(n);
-  IO.printListWithLn(xs);
+  const as = xs.slice().sort((a, b) => b - a);
+  as.unshift(1e9);
+  const ptns = xs.map((x) => {
+    const [ok, _] = Util.binarySearch([0, n + 1], (mid) => as[mid] >= x * 2);
+    return ok
+  })
+  IO.print(Util.sum(ptns).toString());
 }
 
 namespace IO {
@@ -29,7 +34,7 @@ namespace IO {
   // IO 出力系
   const logBuffer: string[] = [];
   const writeLine = (s: string) => logBuffer.push(s)
-  export const print = (data: any) => writeLine(data.toString())
+  export const print = writeLine
   export const printYesNo = (isYes: boolean) => writeLine(isYes ? "Yes" : "No")
   export const printListWithLn = (array: Array<any>) => array.forEach((v) => writeLine(v))
   export const flush = () => console.log(logBuffer.join('\n'))
@@ -43,14 +48,12 @@ namespace Util {
   export const reverse = (s: string) => s.split("").reverse().join("");
   export const range =
     (from: number, to: number) => Array.from({ length: (to - from + 1) }, (_, i) => from + i);
-  export const repeat = (n: number, value: any) => Array.from({ length: n }).fill(value);
   export const sum = (array: number[]) => array.reduce((m, v) => m + v, 0);
-  export const product = (array: number[]) => array.reduce((m, v) => m * v, 1);
   export const max = (array: number[]) => array.reduce((m, v) => Math.max(m, v), 0);
   export const min = (array: number[]) => array.reduce((m, v) => Math.min(m, v), Infinity);
-  export const sortAsc = (array: number[]) => array.slice().sort((a, b) => a - b);
-  export const sortDesc = (array: number[]) => array.slice().sort((a, b) => b - a);
+  export const sort = (array: number[]) => array.slice().sort((a, b) => a - b);
   export const countIf = (array: Array<any>, predicate: (v: any) => boolean) => array.filter(predicate).length;
+  export const repeat = (n: number, value: any) => Array.from({ length: n }).fill(value);
   export const countElements = <T>(array: Array<T>): Map<T, number> => array.reduce((m, v) => m.set(v, m.get(v) ? m.get(v)! + 1 : 1), new Map<T, number>());
   export const rle = <T>(array: Array<T>): Array<[T, number]> => {
     if (array.length === 0) return [];
