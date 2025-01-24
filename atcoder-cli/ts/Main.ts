a47bcceae6e298a2373d0295d431a8b1bc2abdd6
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-24 23:56:05 +0900

    Typescriptのテンプレートを追加

--- atcoder-cli/ts/Main.ts
+++ atcoder-cli/ts/Main.ts
@@ -30,7 +30,7 @@ namespace IO {
   const logBuffer: string[] = [];
   const writeLine = (s: string) => logBuffer.push(s)
   export const print = (data: any) => writeLine(data.toString())
-  export const printYesNo = (isYes: boolean) => writeLine(isYes ? "Yes" : "No")
+  export const printYesNo = (isYes: boolean) => writeLine(Util.boolToYesNo(isYes))
   export const printListWithLn = (array: Array<any>) => array.forEach((v) => writeLine(v))
   export const flush = () => console.log(logBuffer.join('\n'))
 }
@@ -75,6 +75,20 @@ namespace Util {
         Util.binarySearch([mid, ng], check) :
         Util.binarySearch([ok, mid], check);
   }
+  export const manhattanDistance = (point1: [number, number], point2: [number, number]): number => {
+    const [y1, x1] = point1;
+    const [y2, x2] = point2;
+    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+  }
+  // データ変換共通
+  export const boolToYesNo = (bool: boolean): string => (bool ? "Yes" : "No");
+  export const fst3 = <T1, T2, T3>([a, ,]: [T1, T2, T3]): T1 => a;
+  export const snd3 = <T1, T2, T3>([, b,]: [T1, T2, T3]): T2 => b;
+  export const thd3 = <T1, T2, T3>([, , c]: [T1, T2, T3]): T3 => c;
+  export const listToTuple2 = <T>([a, b]: T[]): [T, T] => [a, b];
+  export const listToTuple3 = <T>([a, b, c]: T[]): [T, T, T] => [a, b, c];
+  export const tuple2ToList = <T>([a, b]: [T, T]): T[] => [a, b];
+  export const tuple3ToList = <T>([a, b, c]: [T, T, T]): T[] => [a, b, c];
 }
 
 main();
