e50e1d58aa125e04a66684a5dc93b2115af532b9
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-25 20:18:59 +0900

    提出：https://atcoder.jp/contests/abc386/submissions/62018897

--- kt/abc386/b/Main.kt
+++ kt/abc386/b/Main.kt
@@ -1,6 +1,23 @@
 fun main() {
-    val n = readLine()!!.toInt()
-    val (b, c) = readLine()!!.split(" ").map { it.toInt() }
     val s = readLine()!!
-    println(s)
+    val result =
+            runLengthEncoding(s)
+                    .map { (key, value) -> if (key == '0') (value / 2 + value % 2) else value }
+                    .sum()
+    println(result)
+}
+
+fun runLengthEncoding(s: String): List<Pair<Char, Int>> {
+    val result = mutableListOf<Pair<Char, Int>>()
+    var count = 1
+    for (i in 1 until s.length) {
+        if (s[i] == s[i - 1]) {
+            count++
+        } else {
+            result.add(Pair(s[i - 1], count))
+            count = 1
+        }
+    }
+    result.add(Pair(s.last(), count))
+    return result
 }
