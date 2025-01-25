4596b403fdf1f5028a169d75aa5d0a8a0cd0ed89
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-25 19:59:36 +0900

    提出：https://atcoder.jp/contests/abc387/submissions/62018509

--- kt/abc387/b/Main.kt
+++ kt/abc387/b/Main.kt
@@ -1,6 +1,5 @@
 fun main() {
-    val n = readLine()!!.toInt()
-    val (b, c) = readLine()!!.split(" ").map { it.toInt() }
-    val s = readLine()!!
-    println(s)
+    val x = readLine()!!.toInt()
+    val result = (1..9).flatMap { i -> (1..9).map { j -> i * j } }.filter { it != x }.sum()
+    println(result)
 }
