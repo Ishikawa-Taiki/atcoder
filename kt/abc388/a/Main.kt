57de75820a483ad87f84944758858bcf0dfc505a
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-25 19:35:04 +0900

    提出：https://atcoder.jp/contests/abc388/submissions/62018065

--- kt/abc388/a/Main.kt
+++ kt/abc388/a/Main.kt
@@ -1,7 +1,4 @@
 fun main(args: Array<String>) {
-    val a = readLine()!!.toInt()
-    val (b,c) = readLine()!!.split(" ").map{ it.toInt() }
-    val s = readLine()!!
-    println(a+b+c)
-    println(s)
+    val a = readLine()!!
+    println(a.get(0) + "UPC")
 }
