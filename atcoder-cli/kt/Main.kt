71d106f2e377b9fe1787904c0ab8bf7e0a253f40
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-25 20:19:44 +0900

    runLengthEncoding追加

--- atcoder-cli/kt/Main.kt
+++ atcoder-cli/kt/Main.kt
@@ -4,3 +4,18 @@ fun main() {
     val s = readLine()!!
     println(s)
 }
+
+fun runLengthEncoding(s: String): List<Pair<Char, Int>> {
+    val result = mutableListOf<Pair<Char, Int>>()
+    var count = 1
+    for (i in 1 until s.length) {
+        if (s[i] == s[i - 1]) {
+            count++
+        } else {
+            result.add(Pair(s[i - 1], count))
+            count = 1
+        }
+    }
+    result.add(Pair(s.last(), count))
+    return result
+}
