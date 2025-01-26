70d1566f4109a4bb7c7dfdc1455c060b507d9b53
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-26 14:32:21 +0900

    提出：https://atcoder.jp/contests/abc384/submissions/62118078

--- kt/abc384/b/Main.kt
+++ kt/abc384/b/Main.kt
@@ -1,21 +1,18 @@
 fun main() {
-    val n = readLine()!!.toInt()
-    val (b, c) = readLine()!!.split(" ").map { it.toInt() }
-    val s = readLine()!!
-    println(s)
+    val (n, r) = readIntPair()
+    val xs = (1..n).map { _ -> readIntPair() }
+    val result = xs.fold(r) { acc, (d, a) ->
+        val isRated = when (d) {
+            1 -> 1600 <= acc && acc <= 2799
+            2 -> 1200 <= acc && acc <= 2399
+            else -> false
+        }
+        if (isRated) (acc + a) else acc
+    }
+    println(result)
 }
 
-fun runLengthEncoding(s: String): List<Pair<Char, Int>> {
-    val result = mutableListOf<Pair<Char, Int>>()
-    var count = 1
-    for (i in 1 until s.length) {
-        if (s[i] == s[i - 1]) {
-            count++
-        } else {
-            result.add(Pair(s[i - 1], count))
-            count = 1
-        }
-    }
-    result.add(Pair(s.last(), count))
-    return result
+fun readIntPair(): Pair<Int,Int> {
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    return Pair(a, b) // 2Tupleはこう返せば良い
 }
