775717a5f3bbdca96627f1c1cc2ffeac8c749141
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-01-26 14:16:45 +0900

    提出：https://atcoder.jp/contests/abc384/submissions/62117753

--- kt/abc384/a/Main.kt
+++ kt/abc384/a/Main.kt
@@ -1,21 +1,7 @@
 fun main() {
-    val n = readLine()!!.toInt()
-    val (b, c) = readLine()!!.split(" ").map { it.toInt() }
+    val (n, c1, c2) = readLine()!!.split(" ")
+    val c1c = c1.get(0) // 文字列型から指定位置の文字型の値をとる
+    val c2c = c2.get(0)
     val s = readLine()!!
-    println(s)
-}
-
-fun runLengthEncoding(s: String): List<Pair<Char, Int>> {
-    val result = mutableListOf<Pair<Char, Int>>()
-    var count = 1
-    for (i in 1 until s.length) {
-        if (s[i] == s[i - 1]) {
-            count++
-        } else {
-            result.add(Pair(s[i - 1], count))
-            count = 1
-        }
-    }
-    result.add(Pair(s.last(), count))
-    return result
+    println(s.map { c -> if (c != c1c) c2c else c }.joinToString(""))
 }
