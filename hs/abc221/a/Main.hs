2c7758091e76842b7cc95a1ec4d9d3bb909d81cf
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-02-02 18:19:08 +0900

    提出：https://atcoder.jp/contests/abc221/submissions/62326842

--- hs/abc221/a/Main.hs
+++ hs/abc221/a/Main.hs
@@ -28,15 +28,14 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
   (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  print $ solve a b
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: Int -> Int -> Int
+solve a b = result
   where
-    result = undefined
+    d = a - b
+    result = 32 ^ d
 
 {- Library -}
 -- データ変換共通
