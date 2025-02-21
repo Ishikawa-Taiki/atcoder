44ce60b314cec9dabfcbd9a4bf255871d39242ce
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-02-21 00:22:57 +0000

    https://atcoder.jp/contests/abc205/submissions/62954781

--- hs/abc205/a/Main.hs
+++ hs/abc205/a/Main.hs
@@ -29,15 +29,12 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
   (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  print $ solve a b
 
-solve :: [Int] -> Int
-solve xs = result
+solve a b = result
   where
-    result = undefined
+    result = fromIntegral a * (fromIntegral b / 100)
 
 {- Library -}
 -- データ変換共通
