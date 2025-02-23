484fcadd7ffe6a9f26389b6a8b832bc52c74975e
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-02-23 19:50:12 +0900

    https://atcoder.jp/contests/abc204/submissions/63094718

--- hs/abc204/b/Main.hs
+++ hs/abc204/b/Main.hs
@@ -30,14 +30,14 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Int
+solve xs n = result
   where
-    result = undefined
+    result = sum $ map f xs
+    f = max 0 . subtract 10
 
 {- Library -}
 -- データ変換共通
