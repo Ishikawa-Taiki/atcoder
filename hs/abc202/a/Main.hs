ecb2313daea691f676112fc25fd20b10d6ede41a
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-02-25 00:32:22 +0000

    https://atcoder.jp/contests/abc202/submissions/63136131

--- hs/abc202/a/Main.hs
+++ hs/abc202/a/Main.hs
@@ -29,15 +29,13 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
   print $ solve xs
 
 solve :: [Int] -> Int
 solve xs = result
   where
-    result = undefined
+    result = 21 - sum xs
 
 {- Library -}
 -- データ変換共通
