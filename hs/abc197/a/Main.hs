e45076f4f55815a0d04d6f1fdb569717f1ac65a1
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-03-02 21:32:55 +0900

    https://atcoder.jp/contests/abc197/submissions/63342177

--- hs/abc197/a/Main.hs
+++ hs/abc197/a/Main.hs
@@ -29,15 +29,11 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getLineToString
+  putStrLn $ solve xs
 
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+solve :: String -> String
+solve (x : xs) = xs ++ [x]
 
 {- Library -}
 -- データ変換共通
