9fc89051a7bcf2e0a4d29ad43d023496f0144d53
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-03-05 00:30:24 +0000

    https://atcoder.jp/contests/abc394/submissions/63402125

--- hs/abc394/b/Main.hs
+++ hs/abc394/b/Main.hs
@@ -29,15 +29,11 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  _ <- getLineToInt
+  xs <- getContentsToStringList
+  putStrLn . concatMap snd . sort . map f $ xs
 
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+f s = (length s, s)
 
 {- Library -}
 -- データ変換共通
