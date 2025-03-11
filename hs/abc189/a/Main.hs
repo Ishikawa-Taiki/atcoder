b637ac8c9292f7108a2d5d49b63b64275a7b1092
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-03-11 00:30:15 +0000

    https://atcoder.jp/contests/abc189/submissions/63639133

--- hs/abc189/a/Main.hs
+++ hs/abc189/a/Main.hs
@@ -29,15 +29,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  xs <- fmap S.fromList getLineToString
+  putStrLn . bool "Lost" "Won" $ S.size xs == 1
 
 {- Library -}
 -- データ変換共通
