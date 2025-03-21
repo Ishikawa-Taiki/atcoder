c81cb5d819ec00bbe41bd9064aec686968c6eac7
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-03-21 04:18:36 +0000

    https://atcoder.jp/contests/abc180/submissions/63989656

--- hs/abc180/a/Main.hs
+++ hs/abc180/a/Main.hs
@@ -29,15 +29,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  (n, a, b) <- getLineToIntTuple3
+  print $ n - a + b
 
 {- Library -}
 -- データ変換共通
