6547814dfc43bf608c8f74c37e5caf36e1eefd85
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-04-09 05:02:40 +0000

    https://atcoder.jp/contests/abc162/submissions/64645355

--- hs/abc162/a/Main.hs
+++ hs/abc162/a/Main.hs
@@ -30,15 +30,13 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getLineToString
+  printYesNo $ solve xs
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Char] -> Bool
+solve = result
   where
-    result = undefined
+    result = elem '7'
 
 {- Library -}
 -- データ変換共通
