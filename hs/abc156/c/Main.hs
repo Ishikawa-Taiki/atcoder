b228cfa25a814e21e55dc21ded278d2467e8ecdd
ishikawa_taiki <ishikawataiki.0815@gmail.com>
2025-04-15 13:48:22 +0000

    https://atcoder.jp/contests/abc156/submissions/64873762

--- hs/abc156/c/Main.hs
+++ hs/abc156/c/Main.hs
@@ -31,14 +31,15 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Int
+solve xs n = result
   where
-    result = undefined
+    f p x = (x - p) ^ 2
+    g p = sum $ map (f p) xs
+    result = minimum $ map g [1 .. 100]
 
 {- Library -}
 -- データ変換共通
