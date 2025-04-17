d11d1e89711489319d7c64e15b22c6240d2527a0
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-04-17 12:13:50 +0900

    https://atcoder.jp/contests/abc154/submissions/64933314

--- hs/abc154/c/Main.hs
+++ hs/abc154/c/Main.hs
@@ -31,14 +31,13 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  putStrLn . bool "NO" "YES" $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Bool
+solve xs n = result
   where
-    result = undefined
+    result = n == S.size (S.fromList xs)
 
 {- Library -}
 -- データ変換共通
