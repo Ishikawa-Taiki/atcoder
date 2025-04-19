7dcc3e86b768c19283a30943f81dad1ca9c0b579
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-04-19 22:53:56 +0900

    https://atcoder.jp/contests/abc152/submissions/65041974

--- hs/abc152/c/Main.hs
+++ hs/abc152/c/Main.hs
@@ -31,14 +31,15 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+solve :: [Int] -> Int -> Int
+solve xs n = countIf id . zipWith (==) xs $ scanl1 min xs
+
+-- リスト中の条件を満たす要素の数を返却する
+countIf :: (Eq a) => (a -> Bool) -> [a] -> Int
+countIf f = getSum . foldMap (bool (Sum 0) (Sum 1) . f)
 
 {- Library -}
 -- データ変換共通
