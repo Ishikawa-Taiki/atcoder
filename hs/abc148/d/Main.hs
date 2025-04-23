43cec9cb9e2a226e4b76cfa4cd9b0bd682f36cd7
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-04-23 19:41:45 +0900

    https://atcoder.jp/contests/abc148/submissions/65141352

--- hs/abc148/d/Main.hs
+++ hs/abc148/d/Main.hs
@@ -31,14 +31,15 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Int
+solve xs n = result
   where
-    result = undefined
+    result = bool (n - pred calc) (-1) $ calc == 1
+    calc = foldl f 1 xs
+    f acc x = bool acc (succ acc) $ x == acc
 
 {- Library -}
 -- データ変換共通
