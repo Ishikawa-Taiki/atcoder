3cf759d6fecd2ee923c0085f75a9c04c2ae75623
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-04-25 17:13:46 +0900

    https://atcoder.jp/contests/abc395/submissions/65186997

--- hs/abc395/a/Main.hs
+++ hs/abc395/a/Main.hs
@@ -30,14 +30,13 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  printYesNo $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Bool
+solve xs n = result
   where
-    result = undefined
+    result = xs == sort xs && n == S.size (S.fromList xs)
 
 {- Library -}
 -- データ変換共通
