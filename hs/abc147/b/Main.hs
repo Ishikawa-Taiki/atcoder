25f62e099fd9d2455bbe0efab9ada1d5062cc15d
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-04-28 23:50:58 +0900

    https://atcoder.jp/contests/abc147/submissions/65322770

--- hs/abc147/b/Main.hs
+++ hs/abc147/b/Main.hs
@@ -30,15 +30,21 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
+  xs <- getLineToString
   print $ solve xs
 
-solve :: [Int] -> Int
+solve :: [Char] -> Int
 solve xs = result
   where
-    result = undefined
+    l = length xs
+    hl = l `div` 2
+    prev = take hl xs
+    next = take hl . reverse $ xs
+    result = countIf (uncurry (/=)) $ zip prev next
+
+-- リスト中の条件を満たす要素の数を返却する
+countIf :: (Eq a) => (a -> Bool) -> [a] -> Int
+countIf f = getSum . foldMap (bool (Sum 0) (Sum 1) . f)
 
 {- Library -}
 -- データ変換共通
