514c546175826afae460eba3fb9e2d7c0d4c451b
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-04-29 22:10:35 +0900

    https://atcoder.jp/contests/abc146/submissions/65343026

--- hs/abc146/a/Main.hs
+++ hs/abc146/a/Main.hs
@@ -30,15 +30,14 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
+  xs <- getLineToString
   print $ solve xs
 
-solve :: [Int] -> Int
+solve :: [Char] -> Int
 solve xs = result
   where
-    result = undefined
+    a = reverse ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    result = succ . fromJust . elemIndex xs $ a
 
 {- Library -}
 -- データ変換共通
