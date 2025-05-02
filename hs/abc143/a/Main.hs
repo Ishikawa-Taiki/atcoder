e65f5fd1366fa9a6ae685646e3be5024967f0a7f
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-02 21:48:08 +0900

    https://atcoder.jp/contests/abc143/submissions/65401070

--- hs/abc143/a/Main.hs
+++ hs/abc143/a/Main.hs
@@ -30,15 +30,13 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
   (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  print $ solve a b
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: Int -> Int -> Int
+solve a b = result
   where
-    result = undefined
+    result = max 0 $ a - (b * 2)
 
 {- Library -}
 -- データ変換共通
