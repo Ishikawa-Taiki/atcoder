698623ead166150d6e98b36052371ff3d38ab683
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-05 13:44:03 +0900

    https://atcoder.jp/contests/abc402/submissions/65522790

--- hs/abc402/c/Main.hs
+++ hs/abc402/c/Main.hs
@@ -30,15 +30,30 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  (n, m) <- getLineToIntTuple2
+  xs <- replicateM m $ do
+    (k : as) <- getLineToIntList
+    return (k, as)
+  bs <- getLineToIntList
+  printListWithLn $ solve xs bs n m
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [(Int, [Int])] -> [Int] -> Int -> Int -> [Int]
+solve xs bs n m = result
   where
-    result = undefined
+    b = listArray @UArray (1, n) $ map snd . sort $ zip bs [1 .. n]
+    dishes = maximum . map (b !) . snd <$> xs -- その料理が食べられるようになる日
+    e = countElements dishes -- その日に食べられるようになった料理の数
+    result = reverse . snd . foldl f (0, []) $ [1 .. n]
+    f (count, result) day = (new, new : result)
+      where
+        new = count + M.findWithDefault 0 day e -- その日までに食べられるように料理の数の合計
+
+-- キー毎のカウンター
+type CounterMap k = M.Map k Int
+
+-- リストの各要素を数える
+countElements :: (Ord k) => [k] -> CounterMap k
+countElements = M.fromListWith (+) . map (,1)
 
 {- Library -}
 -- データ変換共通
