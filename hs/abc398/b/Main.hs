1525dbefe56a2f133f638bf2b003797b29681945
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-08 22:08:52 +0900

    https://atcoder.jp/contests/abc398/submissions/65592355

--- hs/abc398/b/Main.hs
+++ hs/abc398/b/Main.hs
@@ -30,15 +30,21 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  printYesNo $ solve xs
 
-solve :: [Int] -> Int
+solve :: [Int] -> Bool
 solve xs = result
   where
-    result = undefined
+    e = M.filter (2 <=) . countElements $ xs
+    result = M.size e >= 2 && any (3 <=) e
+
+-- キー毎のカウンター
+type CounterMap k = M.Map k Int
+
+-- リストの各要素を数える
+countElements :: (Ord k) => [k] -> CounterMap k
+countElements = M.fromListWith (+) . map (,1)
 
 {- Library -}
 -- データ変換共通
