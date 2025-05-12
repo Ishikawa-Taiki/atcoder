a62eb542a66dd41085c1d81ab737f9703c1f453e
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-12 22:00:18 +0900

    https://atcoder.jp/contests/abc139/submissions/65741186

--- hs/abc139/c/Main.hs
+++ hs/abc139/c/Main.hs
@@ -31,14 +31,17 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Int
+solve xs n = result
   where
-    result = undefined
+    result = (\v -> bool (maximum v) 0 $ null v) . map snd . filter fst . rle . zipWith (>=) xs $ tail xs
+
+-- ランレングス圧縮する(リスト上の連続したデータを、データ一つ+連続した長さのリストに変換する)
+rle :: (Eq a) => [a] -> [(a, Int)]
+rle = map (\x -> (head x, length x)) . group
 
 {- Library -}
 -- データ変換共通
