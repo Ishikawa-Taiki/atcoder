d64ed799fe3d5b00c04915a279c990c19b661c7c
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-12 21:53:44 +0900

    https://atcoder.jp/contests/abc139/submissions/65741032

--- hs/abc139/b/Main.hs
+++ hs/abc139/b/Main.hs
@@ -30,15 +30,17 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
   (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  print $ solve a b
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: Int -> Int -> Int
+solve a b = result
   where
-    result = undefined
+    result = pred b `ceilDiv` pred a
+
+-- 繰り上げ除算
+ceilDiv :: (Integral a) => a -> a -> a
+ceilDiv x y = (x + pred y) `div` y
 
 {- Library -}
 -- データ変換共通
