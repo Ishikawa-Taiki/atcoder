3d3002a76d2240ad80a78f23090c555089a6bc92
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-16 22:49:23 +0900

    https://atcoder.jp/contests/abc405/submissions/65830188

--- hs/abc405/c/Main.hs
+++ hs/abc405/c/Main.hs
@@ -30,15 +30,14 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  n <- getLineToInteger
+  xs <- getLineToIntegerList
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Integer] -> Integer -> Integer
+solve xs n = result
   where
-    result = undefined
+    result = sum . zipWith (*) xs . reverse . init . scanl1 (+) . reverse $ xs
 
 {- Library -}
 -- データ変換共通
