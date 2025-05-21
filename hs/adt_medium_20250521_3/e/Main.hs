68b0d394f6eda468880292009df06df4309edbc7
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-21 22:23:54 +0900

    https://atcoder.jp/contests/adt_medium_20250521_3/submissions/66025004

--- hs/adt_medium_20250521_3/e/Main.hs
+++ hs/adt_medium_20250521_3/e/Main.hs
@@ -31,14 +31,17 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  printListWithSpace $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> [Int]
+solve xs n = result
   where
-    result = undefined
+    result = reverse . fst . foldl f ([], M.empty) $ zip [1 ..] xs
+    f (acc, m) (i, x) =
+      let m' = M.insertWith (++) x [i] m
+          search = head $ M.findWithDefault [-1] x m
+       in (search : acc, m')
 
 {- Library -}
 -- データ変換共通
