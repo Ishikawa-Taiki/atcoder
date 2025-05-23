5fd70d4d6f42307c2a1e0679394e26d905fb0125
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-23 23:12:03 +0900

    https://atcoder.jp/contests/adt_medium_20250521_1/submissions/66062268

--- hs/adt_medium_20250521_1/d/Main.hs
+++ hs/adt_medium_20250521_1/d/Main.hs
@@ -30,15 +30,21 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  (n, m) <- getLineToIntTuple2
+  as <- getLineToIntList
+  bs <- getLineToIntList
+  printYesNo $ solve as bs n m
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> [Int] -> Int -> Int -> Bool
+solve as bs n m = result
   where
-    result = undefined
+    a = (,'a') <$> as
+    b = (,'b') <$> bs
+    result = any (\(c,i) -> c == 'a' && i >= 2) . rle . map snd . sort $ a ++ b
+
+-- ランレングス圧縮する(リスト上の連続したデータを、データ一つ+連続した長さのリストに変換する)
+rle :: (Eq a) => [a] -> [(a, Int)]
+rle = map (\x -> (head x, length x)) . group
 
 {- Library -}
 -- データ変換共通
