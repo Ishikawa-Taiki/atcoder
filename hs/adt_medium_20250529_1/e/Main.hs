8755cf6cdfe8b3676de5d82ec8d78f201b0f6a57
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-05-30 22:52:53 +0900

    https://atcoder.jp/contests/adt_medium_20250529_1/submissions/66277295

--- hs/adt_medium_20250529_1/e/Main.hs
+++ hs/adt_medium_20250529_1/e/Main.hs
@@ -30,15 +30,33 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  (n, k) <- getLineToIntTuple2
+  xs <- getLineToString
+  print $ solve xs n k
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Char] -> Int -> Int -> Int
+solve xs n k = result
   where
-    result = undefined
+    result = length [ptn | ptn <- uniquePermutations xs, not (containsPalindromeNK n k ptn)]
+
+-- リスト中の条件を満たす要素の数を返却する
+countIf :: (Eq a) => (a -> Bool) -> [a] -> Int
+countIf f = getSum . foldMap (bool (Sum 0) (Sum 1) . f)
+
+-- リストの要素を並び替えた全組み合わせを返却する(重複項目は除く) ※ 標準の permutations が微妙らしい(?)
+uniquePermutations :: (Eq a, Show a) => [a] -> [[a]]
+uniquePermutations [] = [[]]
+uniquePermutations s = do
+  x <- nub s -- リストモナドなので、xはnubが返す各要素に順に取り出す変数になる
+  map (x :) . uniquePermutations $ delete x s -- 上記より、この行が要素数分実行される形になる deleteは最初に見つかったxを消すので組み合わせを作れる
+
+-- 長さNの文字列中に長さKの回文が含まれているかを返却する(N>=K)　※ nは文字列から求められるので消したい気もする
+containsPalindromeNK :: Int -> Int -> String -> Bool
+containsPalindromeNK n k = any (isPalindrome . take k) . take (n - k + 1) . tails -- K文字の部分文字列として考えられるものを列挙しながら、それらが回文であるかどうかを確認する
+
+-- 回文かどうかを返却する
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == reverse xs
 
 {- Library -}
 -- データ変換共通
