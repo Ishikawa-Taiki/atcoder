f5448ccd291b54708fa3bf4892bd86862d74f0ff
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-06-07 16:48:57 +0900

    https://atcoder.jp/contests/adt_medium_20250603_3/submissions/66498253

--- hs/adt_medium_20250603_3/e/Main.hs
+++ hs/adt_medium_20250603_3/e/Main.hs
@@ -19,6 +19,7 @@ import Data.Bool (bool)
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
 import Data.Char (digitToInt, intToDigit, isLower, isUpper, toLower, toUpper)
+import Data.Graph (Bounds, Graph, Vertex, buildG, dff, indegree, outdegree, reachable)
 import Data.List
 import Data.Map qualified as M
 import Data.Maybe (fromJust, fromMaybe)
@@ -31,14 +32,20 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getContentsToIntTuples2
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [(Int, Int)] -> Int -> Int
+solve xs n = result
   where
-    result = undefined
+    maxFloor = maximum $ concatMap (\(a, b) -> [a, b]) xs
+    g = adjacencyListUndirected (1, maxFloor) xs
+    result = maximum $ reachable g 1
+
+{- 無効グラフ -}
+-- 隣接リスト表現(Data.Graphベース)
+adjacencyListUndirected :: Foldable t => Bounds -> t (Vertex, Vertex) -> Graph
+adjacencyListUndirected bounds pairs = buildG bounds $ concatMap (\x -> [x, swap x]) pairs
 
 {- Library -}
 -- データ変換共通
