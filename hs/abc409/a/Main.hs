c61ef98d1810763a12ed989979bc82ff36d49b73
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-06-08 22:17:02 +0900

    https://atcoder.jp/contests/abc409/submissions/66609254

--- hs/abc409/a/Main.hs
+++ hs/abc409/a/Main.hs
@@ -31,14 +31,14 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getLineToString
+  ys <- getLineToString
+  printYesNo $ solve xs ys n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Char] -> [Char] -> Int -> Bool
+solve xs ys n = result
   where
-    result = undefined
+    result = or $ zipWith (\x y -> x == 'o' && y == 'o') xs ys
 
 {- Library -}
 -- データ変換共通
