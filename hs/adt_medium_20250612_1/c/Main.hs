bf4388686da47ba52c128e5c39ab1cae8ba549d0
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-06-12 23:06:58 +0900

    https://atcoder.jp/contests/adt_medium_20250612_1/submissions/66686197

--- hs/adt_medium_20250612_1/c/Main.hs
+++ hs/adt_medium_20250612_1/c/Main.hs
@@ -31,14 +31,13 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Int
+solve xs n = result
   where
-    result = undefined
+    result = length . takeWhile (4 <=) . reverse . scanl1 (+) . reverse $ xs
 
 {- Library -}
 -- データ変換共通
