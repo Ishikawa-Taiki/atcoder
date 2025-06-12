c448279b574306dd79c158e6375721986c60f429
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-06-12 23:13:00 +0900

    https://atcoder.jp/contests/adt_medium_20250612_1/submissions/66686308

--- hs/adt_medium_20250612_1/d/Main.hs
+++ hs/adt_medium_20250612_1/d/Main.hs
@@ -31,14 +31,28 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getContentsToStringList
+  putStrLn $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [String] -> Int -> String
+solve xs n = result
   where
-    result = undefined
+    m =
+      M.fromList $
+        zip
+          [ "-----",
+            ".----",
+            "..---",
+            "...--",
+            "....-",
+            ".....",
+            "-....",
+            "--...",
+            "---..",
+            "----."
+          ]
+          ['0' .. '9']
+    result = map (m M.!) xs
 
 {- Library -}
 -- データ変換共通
