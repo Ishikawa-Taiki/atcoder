b7db080cf909bd525ee27935b2d2863510af2a2c
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-06-15 22:47:25 +0900

    https://atcoder.jp/contests/adt_medium_20250612_2/submissions/66813054

--- hs/adt_medium_20250612_2/d/Main.hs
+++ hs/adt_medium_20250612_2/d/Main.hs
@@ -30,15 +30,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  xs <- fmap (map head) getContentsToIntMatrix
+  printListWithLn $ reverse xs
 
 {- Library -}
 -- データ変換共通
