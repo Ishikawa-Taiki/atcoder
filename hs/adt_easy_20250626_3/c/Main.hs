77029912f7306dd890c9c27ae24812cf43158409
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-07-03 22:25:48 +0900

    https://atcoder.jp/contests/adt_easy_20250626_3/submissions/67261829

--- hs/adt_easy_20250626_3/c/Main.hs
+++ hs/adt_easy_20250626_3/c/Main.hs
@@ -31,14 +31,8 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  print $ snd . (!! 1) $ sortBy (flip compare) $ zip xs [1 ..]
 
 {- Library -}
 -- データ変換共通
