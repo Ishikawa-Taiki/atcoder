fda89f34dc159143a6a358deb5e2f523c06ae22d
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-07-03 22:22:18 +0900

    https://atcoder.jp/contests/adt_easy_20250626_3/submissions/67261756

--- hs/adt_easy_20250626_3/b/Main.hs
+++ hs/adt_easy_20250626_3/b/Main.hs
@@ -30,15 +30,12 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getContentsToStringList
+  print $ countIf (== "Takahashi") xs
 
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+-- リスト中の条件を満たす要素の数を返却する
+countIf :: (Eq a) => (a -> Bool) -> [a] -> Int
+countIf f = getSum . foldMap (bool (Sum 0) (Sum 1) . f)
 
 {- Library -}
 -- データ変換共通
