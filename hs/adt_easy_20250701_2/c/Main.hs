96125ed72446b11f931a359e87e35fe99f9c4a3f
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-07-06 22:54:50 +0900

    https://atcoder.jp/contests/adt_easy_20250701_2/submissions/67393187

--- hs/adt_easy_20250701_2/c/Main.hs
+++ hs/adt_easy_20250701_2/c/Main.hs
@@ -30,15 +30,21 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  (n, m) <- getLineToIntTuple2
+  s <- getLineToString
+  t <- getLineToString
+  print $ solve s t n m
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: String -> String -> Int -> Int -> Int
+solve s t n m = result
   where
-    result = undefined
+    isPre = all (uncurry (==)) $ zip t s
+    isSuf = all (uncurry (==)) $ zip (reverse t) (reverse s)
+    result = case (isPre, isSuf) of
+      (True, True) -> 0
+      (True, False) -> 1
+      (False, True) -> 2
+      (False, False) -> 3
 
 {- Library -}
 -- データ変換共通
