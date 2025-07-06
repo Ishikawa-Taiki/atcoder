1ba8bbbe8fa3d779ce86ba1a4b66e748e3187d51
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-07-06 22:50:09 +0900

    https://atcoder.jp/contests/adt_easy_20250701_2/submissions/67392821

--- hs/adt_easy_20250701_2/b/Main.hs
+++ hs/adt_easy_20250701_2/b/Main.hs
@@ -30,15 +30,13 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getLineToString
+  putChar $ solve xs
 
-solve :: [Int] -> Int
+solve :: [Char] -> Char
 solve xs = result
   where
-    result = undefined
+    result = xs !! (length xs `div` 2)
 
 {- Library -}
 -- データ変換共通
