0df7ddd9de4b2adafd87883db1ae00595f435e59
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-07-08 22:21:19 +0900

    https://atcoder.jp/contests/adt_easy_20250701_3/submissions/67432479

--- hs/adt_easy_20250701_3/b/Main.hs
+++ hs/adt_easy_20250701_3/b/Main.hs
@@ -30,15 +30,14 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
+  (n, k) <- getLineToIntTuple2
   xs <- getLineToIntList
-  print $ solve xs
+  printListWithSpace $ solve xs n k
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [Int] -> Int -> Int -> [Int]
+solve xs n k = result
   where
-    result = undefined
+    result = (++ replicate (min k n) 0) . drop k $ xs
 
 {- Library -}
 -- データ変換共通
