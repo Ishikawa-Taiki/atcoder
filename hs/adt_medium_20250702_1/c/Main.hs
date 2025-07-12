802957300ab75b40e8e81ea678998748d5d36e9c
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-07-12 22:56:23 +0900

    https://atcoder.jp/contests/adt_medium_20250702_1/submissions/67561285

--- hs/adt_medium_20250702_1/c/Main.hs
+++ hs/adt_medium_20250702_1/c/Main.hs
@@ -30,15 +30,9 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  (h, w) <- getLineToIntTuple2
+  xxs <- getContentsToIntMatrix
+  printMatrix $ transpose xxs
 
 {- Library -}
 -- データ変換共通
