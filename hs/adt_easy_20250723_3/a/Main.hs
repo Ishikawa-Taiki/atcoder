fc42a409e634ddadff1590936fca16e4cde556a5
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-08-02 23:11:14 +0900

    https://atcoder.jp/contests/adt_easy_20250723_3/submissions/68166966

--- hs/adt_easy_20250723_3/a/Main.hs
+++ hs/adt_easy_20250723_3/a/Main.hs
@@ -30,15 +30,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  xs <- getLineToString
+  printYesNo . and . zipWith (==) (reverse "san") $ reverse xs
 
 {- Library -}
 -- データ変換共通
