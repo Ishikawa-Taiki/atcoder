c42cf32d1f1bb01a6ed1ff2385be6db02aa773e6
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-08-02 23:24:12 +0900

    https://atcoder.jp/contests/adt_easy_20250723_3/submissions/68167782

--- hs/adt_easy_20250723_3/b/Main.hs
+++ hs/adt_easy_20250723_3/b/Main.hs
@@ -31,14 +31,7 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  putStrLn $ bool (replicate ((n -2) `div` 2) '-' ++ "==" ++ replicate ((n -2) `div` 2) '-') (replicate (n `div` 2) '-' ++ "=" ++ replicate (n `div` 2) '-') $ odd n
 
 {- Library -}
 -- データ変換共通
