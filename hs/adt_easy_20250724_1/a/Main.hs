bc82e190333bf4fd2814ea2d033765089e7690c5
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-08-03 22:12:32 +0900

    https://atcoder.jp/contests/adt_easy_20250724_1/submissions/68195240

--- hs/adt_easy_20250724_1/a/Main.hs
+++ hs/adt_easy_20250724_1/a/Main.hs
@@ -31,14 +31,8 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  xs <- getLineToString
+  printYesNo $ 'o' == xs !! pred n
 
 {- Library -}
 -- データ変換共通
