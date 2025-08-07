17aee0ab2ab1208c2319ac59029b0bbf2bb52f45
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-08-07 22:28:21 +0900

    https://atcoder.jp/contests/adt_easy_20250805_2/submissions/68275207

--- hs/adt_easy_20250805_2/a/Main.hs
+++ hs/adt_easy_20250805_2/a/Main.hs
@@ -31,14 +31,17 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getContentsToIntTuples2
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [(Int, Int)] -> Int -> Int
+solve xs n = result
   where
-    result = undefined
+    result = countIf (uncurry (<)) xs
+
+-- リスト中の条件を満たす要素の数を返却する
+countIf :: (Eq a) => (a -> Bool) -> [a] -> Int
+countIf f = getSum . foldMap (bool (Sum 0) (Sum 1) . f)
 
 {- Library -}
 -- データ変換共通
