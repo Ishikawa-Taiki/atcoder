27ba6b8d773e1666c484139eecd242b62a3e7f3c
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-08-15 22:34:30 +0900

    https://atcoder.jp/contests/abc418/submissions/68493652

--- hs/abc418/a/Main.hs
+++ hs/abc418/a/Main.hs
@@ -31,14 +31,8 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  xs <- getLineToString
+  printYesNo $ "tea" == drop (n -3) xs
 
 {- Library -}
 -- データ変換共通
