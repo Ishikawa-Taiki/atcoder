e8254029afc9d7cad3a774c52c1cdaa0dcdd7631
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-08-21 22:27:29 +0900

    https://atcoder.jp/contests/adt_easy_20250812_3/submissions/68678754!

--- hs/adt_easy_20250812_3/e/Main.hs
+++ hs/adt_easy_20250812_3/e/Main.hs
@@ -31,14 +31,9 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  as <- getLineToIntList
+  bs <- getLineToIntList
+  print $ maximum as + maximum bs
 
 {- Library -}
 -- データ変換共通
