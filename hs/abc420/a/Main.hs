1749587c24b9f8247348e28d069c3c4bacae4e95
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-10-10 22:46:53 +0900

    https://atcoder.jp/contests/abc420/submissions/70005732

--- hs/abc420/a/Main.hs
+++ hs/abc420/a/Main.hs
@@ -30,15 +30,8 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  (x, y) <- getLineToIntTuple2
+  print . succ $ (pred x + y) `mod` 12
 
 {- Library -}
 -- データ変換共通
