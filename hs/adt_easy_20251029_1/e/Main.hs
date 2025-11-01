46f718836cbd939a51d3d3fe7203837d5edca3cc
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-11-01 16:18:07 +0900

    https://atcoder.jp/contests/adt_easy_20251029_1/submissions/70585030

--- hs/adt_easy_20251029_1/e/Main.hs
+++ hs/adt_easy_20251029_1/e/Main.hs
@@ -30,15 +30,13 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  (n, m) <- getLineToIntTuple2
+  printMatrix $ solve n m
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: Int -> Int -> [[Int]]
+solve n m = result
   where
-    result = undefined
+    result = sort . filter ((== n) . length) $ subsequences [1 .. m]
 
 {- Library -}
 -- データ変換共通
