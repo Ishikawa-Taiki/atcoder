6c3cc6f201302919359954267894c17ca58ab376
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-11-04 21:14:27 +0900

    https://atcoder.jp/contests/adt_easy_20251030_1/submissions/70685067

--- hs/adt_easy_20251030_1/a/Main.hs
+++ hs/adt_easy_20251030_1/a/Main.hs
@@ -31,14 +31,13 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
   xs <- getLineToIntList
   print $ solve xs
 
 solve :: [Int] -> Int
-solve xs = result
+solve (x : xs) = result
   where
-    result = undefined
+    result = maybe (-1) (+ 2) . findIndex (x <) $ xs
 
 {- Library -}
 -- データ変換共通
