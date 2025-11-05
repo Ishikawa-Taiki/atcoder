02f5862c0e70853fa61936c85dc8209f1d5261fb
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-11-05 22:25:17 +0900

    https://atcoder.jp/contests/adt_easy_20251030_1/submissions/70705745

--- hs/adt_easy_20251030_1/e/Main.hs
+++ hs/adt_easy_20251030_1/e/Main.hs
@@ -30,15 +30,16 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
+  xs <- getLineToString
   print $ solve xs
 
-solve :: [Int] -> Int
+solve :: String -> Int
 solve xs = result
   where
-    result = undefined
+    result = fst . foldl f (0, 0) . map digitToInt . reverse $ xs
+    f (total, bCount) x = (total + succ currentB, (bCount + currentB) `mod` 10)
+      where
+        currentB = if x >= bCount then x - bCount else (10 + x) - bCount
 
 {- Library -}
 -- データ変換共通
