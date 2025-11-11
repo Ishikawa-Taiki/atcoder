3338ea2aecfc7170793cb6f615a147ab45957185
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-11-11 18:08:59 +0900

    https://atcoder.jp/contests/adt_easy_20251105_1/submissions/70866088

--- hs/adt_easy_20251105_1/a/Main.hs
+++ hs/adt_easy_20251105_1/a/Main.hs
@@ -30,15 +30,7 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  getLineToString >>= putStrLn . filter (== '2')
 
 {- Library -}
 -- データ変換共通
