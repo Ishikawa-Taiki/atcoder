6d9bb0db599c2529e9c357d930006eef20fea94d
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-12-18 21:59:34 +0900

    https://atcoder.jp/contests/adt_medium_20251216_2/submissions/71792869

--- hs/adt_medium_20251216_2/c/Main.hs
+++ hs/adt_medium_20251216_2/c/Main.hs
@@ -31,14 +31,21 @@ import Debug.Trace (trace)
 main :: IO ()
 main = do
   n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
+  xs <- getContentsToIntTuples2
+  print $ solve xs n
 
-solve :: [Int] -> Int
-solve xs = result
+solve :: [(Int, Int)] -> Int -> Double
+solve xs n = result
   where
-    result = undefined
+    ys = bimap fromIntegral fromIntegral <$> xs
+    result = maximum $ distanceTwoPoints <$> ys <*> ys
+
+-- 二次元平面上の2点間のユークリッド距離を計算する(近似値)
+distanceTwoPoints :: (Double, Double) -> (Double, Double) -> Double
+distanceTwoPoints (x1, y1) (x2, y2) =
+  let distanceX = abs (x2 - x1)
+      distanceY = abs (y2 - y1)
+   in sqrt (distanceX ^ 2 + distanceY ^ 2)
 
 {- Library -}
 -- データ変換共通
