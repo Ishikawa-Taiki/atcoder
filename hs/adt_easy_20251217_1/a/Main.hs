cf1e20ac6f30d78aeecaceb6f60fcdfe6d9aa6b3
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-12-23 22:30:33 +0900

    https://atcoder.jp/contests/adt_easy_20251217_1/submissions/71937374

--- hs/adt_easy_20251217_1/a/Main.hs
+++ hs/adt_easy_20251217_1/a/Main.hs
@@ -30,15 +30,7 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  n <- getLineToInt
-  (a, b) <- getLineToIntTuple2
-  xs <- getLineToIntList
-  print $ solve xs
-
-solve :: [Int] -> Int
-solve xs = result
-  where
-    result = undefined
+  putChar . head . S.toList . S.difference (S.fromList ['a' .. 'z']) . S.fromList =<< getLineToString
 
 {- Library -}
 -- データ変換共通
